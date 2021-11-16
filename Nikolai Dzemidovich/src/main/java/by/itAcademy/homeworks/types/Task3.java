ce046a5f7938ad6ee92de66bd385d75cee46d5bb
NickolaiDemidovich <94470072+NickolaiDemidovich@users.noreply.github.com>
2021-11-16 19:47:16 +0300

    Update Task3.java

--- Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/types/Task3.java
+++ Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/types/Task3.java
@@ -1,4 +1,24 @@
 package by.itAcademy.homeworks.types;
 
 public class Task3 {
+     public static void main(String[] args) {
+       
+        byte b = 0x55;
+        short s = 0x55ff;
+        int i = 1000000;
+        long l = 0xffffffffL;
+        char c = 'a';
+        float f = .25f;
+        double d = .00001234;
+        boolean bool = true;
+       
+        System.out.println("byte = " + b);
+        System.out.println("short = " + s);
+        System.out.println("int = " + i);
+        System.out.println("long = " + l);
+        System.out.println("char = " + c);
+        System.out.println("float = " + f);
+        System.out.println("double = " + d);
+        System.out.println("boolean = " + bool);
+    }
 }
