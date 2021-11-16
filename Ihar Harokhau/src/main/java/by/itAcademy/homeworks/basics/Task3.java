932c6e2607a7508258197c2cee65684b09f8d189
Ihar_Harokhau <78763230+IharHarokhau@users.noreply.github.com>
2021-11-16 23:04:40 +0300

    Update Task3.java

--- Ihar Harokhau/src/main/java/by/itAcademy/homeworks/basics/Task3.java
+++ Ihar Harokhau/src/main/java/by/itAcademy/homeworks/basics/Task3.java
@@ -1,4 +1,12 @@
 package by.itAcademy.homeworks.basics;
 
+/*
+    Задание 3
+    На примере задания 2 добавить к основному сообщению дополнительное “I like Java!”, откомпилировать его и запустить.
+ */
+
 public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Hello Belarus!\nI like Java!");
+    }
 }
