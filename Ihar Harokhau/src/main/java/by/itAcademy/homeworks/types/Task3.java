bf6245d0722b7eded9d9ecd65c1545adbfcd0167
Ihar_Harokhau <78763230+IharHarokhau@users.noreply.github.com>
2021-11-16 23:34:41 +0300

    Update Task3.java

--- Ihar Harokhau/src/main/java/by/itAcademy/homeworks/types/Task3.java
+++ Ihar Harokhau/src/main/java/by/itAcademy/homeworks/types/Task3.java
@@ -1,4 +1,21 @@
 package by.itAcademy.homeworks.types;
 
+/*
+    Задание 3
+    Написать код, который выведет значения переменных на экран.
+ */
+
 public class Task3 {
+    public static void main(String[] args) {
+        byte b = 0x55;
+        short s = 0x55ff;
+        int i = 1000000;
+        long l = 0xffffffffL;
+        char c = 'a';
+        float f = .25f;
+        double d = .00001234;
+        boolean bool = true;
+
+        System.out.println(b + "\n" + s + "\n" + i + "\n" + l + "\n" + c + "\n" + f + "\n" + d + "\n" + bool);
+    }
 }
