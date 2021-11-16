559cfea4ba05b48662971517555937509ef0c613
NickolaiDemidovich <94470072+NickolaiDemidovich@users.noreply.github.com>
2021-11-16 22:28:53 +0300

    Update Task6.java

--- Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/operators/Task6.java
+++ Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/operators/Task6.java
@@ -1,4 +1,22 @@
 package by.itAcademy.homeworks.operators;
 
+//Создайте метод с одним целочисленным параметром. Метод должен определить, является ли последняя цифра числа семеркой и вернуть boolean значение.
+
+import java.util.Scanner;
+
 public class Task6 {
+  
+  public static void main(String[] args) {
+
+        Scanner scr = new Scanner(System.in);
+
+        int number = scr.nextInt();
+        boolean last = (number % 10) == 7;
+
+        if (last) {
+            System.out.println(last);
+        } else {
+            System.out.println(last);
+        }
+    }
 }
