02b84ee7c35eb1bdd0d302ec5386cef6275e7606
Sergey Khomiakov <tnkmaze4.0@gmail.com>
2021-11-16 21:00:18 +0300

    First commit

--- Sergey Khomiakov/src/main/java/by/itAcademy/homeworks/basics/Task1.java
+++ Sergey Khomiakov/src/main/java/by/itAcademy/homeworks/basics/Task1.java
@@ -1,4 +1,11 @@
 package by.itAcademy.homeworks.basics;
 
+//Набрать приведенный пример HelloWorld.java, откомпилировать его и запустить.
+
 public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+
+
 }
