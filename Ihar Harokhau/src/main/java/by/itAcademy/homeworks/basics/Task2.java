ce638091201c20a8ea5be2156d67b8700fbc40ab
Ihar_Harokhau <78763230+IharHarokhau@users.noreply.github.com>
2021-11-16 22:58:33 +0300

    Update Task2.java

--- Ihar Harokhau/src/main/java/by/itAcademy/homeworks/basics/Task2.java
+++ Ihar Harokhau/src/main/java/by/itAcademy/homeworks/basics/Task2.java
@@ -1,4 +1,12 @@
 package by.itAcademy.homeworks.basics;
 
-public class Task2 {
+/*
+    Задание 2
+    На примере задания 1 изменить сообщение на "Hello Belarus!", откомпилировать его и запустить.
+ */
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Belarus!");
+    }
 }
