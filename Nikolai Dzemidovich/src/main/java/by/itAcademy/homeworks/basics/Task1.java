4848e86c4a5da0ae451dad54a29124fd4ddbee78
NickolaiDemidovich <94470072+NickolaiDemidovich@users.noreply.github.com>
2021-11-16 22:29:23 +0300

    Update Task1.java

--- Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/basics/Task1.java
+++ Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/basics/Task1.java
@@ -1,5 +1,5 @@
 package by.itAcademy.homeworks.basics;
-
+//Набрать приведенный пример HelloWorld.java, откомпилировать его и запустить.
 public class Task1 {
   
    public static void main(String[] args) {
