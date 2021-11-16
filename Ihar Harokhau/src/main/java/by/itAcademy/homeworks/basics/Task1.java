5ee5828e6cc8dacb7fcd366a0001dcc3a371999f
Ihar_Harokhau <78763230+IharHarokhau@users.noreply.github.com>
2021-11-16 22:54:54 +0300

    Update Task1.java

--- Ihar Harokhau/src/main/java/by/itAcademy/homeworks/basics/Task1.java
+++ Ihar Harokhau/src/main/java/by/itAcademy/homeworks/basics/Task1.java
@@ -1,4 +1,12 @@
 package by.itAcademy.homeworks.basics;
 
+/*
+    Задание 1
+    Набрать приведенный пример HelloWorld, откомпилировать его и запустить.
+ */
+
 public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
