db755d39d1f16c614c6634e90cd62ff1e4a05654
NickolaiDemidovich <94470072+NickolaiDemidovich@users.noreply.github.com>
2021-11-16 22:30:11 +0300

    Update Task3.java

--- Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/basics/Task3.java
+++ Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/basics/Task3.java
@@ -1,5 +1,5 @@
 package by.itAcademy.homeworks.basics;
-
+//В набранном из задания 2 примере добавить к основному сообщению дополнительное “I like Java!”. Откомпилировать пример и запустить.
 public class Task3 {
   
       public static void main(String[] args) {
