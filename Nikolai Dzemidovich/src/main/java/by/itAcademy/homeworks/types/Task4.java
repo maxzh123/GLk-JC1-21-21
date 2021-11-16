007aaa92d7f9149b0bd0778d3bb54db10a95a643
NickolaiDemidovich <94470072+NickolaiDemidovich@users.noreply.github.com>
2021-11-16 19:53:10 +0300

    Update Task4.java

--- Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/types/Task4.java
+++ Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/types/Task4.java
@@ -1,4 +1,21 @@
 package by.itAcademy.homeworks.types;
 
+import java.util.Scanner;
+
 public class Task4 {
+  
+  public static void main(String[] args) {
+
+        Scanner scr = new Scanner(System.in);
+
+        System.out.println("Введите первое число");
+        int firstNumber = scr.nextInt();
+
+        System.out.println("Введите второе число");
+        int secondNumber = scr.nextInt();
+
+        System.out.println((firstNumber+secondNumber) + (firstNumber*secondNumber));
+
+
+    }
 }
