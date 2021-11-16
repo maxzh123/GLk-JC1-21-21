46ab8ef678c0bd2665febb59ab90dbade0b7bbd0
Sergey Khomiakov <tnkmaze4.0@gmail.com>
2021-11-16 21:14:03 +0300

    Second commit

--- Sergey Khomiakov/src/main/java/by/itAcademy/homeworks/basics/Task2.java
+++ Sergey Khomiakov/src/main/java/by/itAcademy/homeworks/basics/Task2.java
@@ -1,4 +1,10 @@
 package by.itAcademy.homeworks.basics;
 
+//В набранном из задания 1 примере изменить сообщение на “Hello Belarus!”. Откомпилировать пример и запустить.
+
 public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Belarus!");
+    }
+
 }
--- Sergey Khomiakov/src/main/java/by/itAcademy/homeworks/basics/Task3.java
+++ Sergey Khomiakov/src/main/java/by/itAcademy/homeworks/basics/Task3.java
@@ -1,4 +1,11 @@
 package by.itAcademy.homeworks.basics;
 
+/*В набранном из задания 2 примере добавить к основному сообщению дополнительное “I like Java!”.
+Откомпилировать пример и запустить.*/
+
 public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Hello Belarus!");
+        System.out.println("I Like Java");
+    }
 }
