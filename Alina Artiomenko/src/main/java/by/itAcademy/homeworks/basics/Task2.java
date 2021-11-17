24b1309db02f360243b22d89589ee9e021b9bb6f
Meyhe <89459095+Meyhe@users.noreply.github.com>
2021-11-17 09:09:46 +0300

    Update Task2.java

--- Alina Artiomenko/src/main/java/by/itAcademy/homeworks/basics/Task2.java
+++ Alina Artiomenko/src/main/java/by/itAcademy/homeworks/basics/Task2.java
@@ -1,4 +1,11 @@
 package by.itAcademy.homeworks.basics;
 
+/** Задание 02
+* Вывести в консоль Hello Belarus!
+**/
+
 public class Task2 {
+  public static void main(String[] args){
+    System.out.println("Hello Belarus!");
+  }
 }
