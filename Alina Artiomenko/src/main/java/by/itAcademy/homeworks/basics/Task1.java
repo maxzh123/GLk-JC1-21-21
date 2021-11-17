1f674d53705b716e684872696ea4c9240b843f5d
Meyhe <89459095+Meyhe@users.noreply.github.com>
2021-11-17 09:07:56 +0300

    Update Task1.java

--- Alina Artiomenko/src/main/java/by/itAcademy/homeworks/basics/Task1.java
+++ Alina Artiomenko/src/main/java/by/itAcademy/homeworks/basics/Task1.java
@@ -1,4 +1,11 @@
 package by.itAcademy.homeworks.basics;
 
+/** Задание 01
+*Вывести в консоль Hello World!
+**/
+
 public class Task1 {
+  public static void main(String[] args){
+    System.out.println("Hello World!");
+  }
 }
