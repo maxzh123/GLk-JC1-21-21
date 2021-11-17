ec043e6092036b6d172aa872025ac4753f54bb21
Dzmitry1405 <vdn_13@mail.ru>
2021-11-17 16:13:13 +0300

    Dzmitry1405

--- Dzmitry Vasilchyk/src/main/java/by/itAcademy/homeworks/basics/Task1.java
+++ Dzmitry Vasilchyk/src/main/java/by/itAcademy/homeworks/basics/Task1.java
@@ -1,4 +1,10 @@
 package by.itAcademy.homeworks.basics;
+/**Задание01:
+ вывести на экран HelloWorld.java
+ **/
 
 public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld.java");
+    }
 }
