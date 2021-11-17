6d5efa776693a0aaf8da0f618d8e7ca33f770993
EkaterinaCherkas <94483993+EkaterinaCherkas@users.noreply.github.com>
2021-11-18 01:51:33 +0300

    Update Task1.java

--- Ekaterina Cherkas/src/main/java/by/itAcademy/homeworks/basics/Task1.java
+++ Ekaterina Cherkas/src/main/java/by/itAcademy/homeworks/basics/Task1.java
@@ -1,4 +1,13 @@
 package by.itAcademy.homeworks.basics;
 
+/** Задание 1
+ *
+ * Набрать приведенный пример HelloWorld, откомпилировать его и запустить.
+ */
+
+
 public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
 }
