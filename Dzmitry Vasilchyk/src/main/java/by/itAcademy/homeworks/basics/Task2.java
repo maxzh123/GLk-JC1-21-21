6cd7663344d967b600f9e67c397787b190b4a237
Dzmitry1405 <vdn_13@mail.ru>
2021-11-17 16:20:12 +0300

    Task2.java

--- Dzmitry Vasilchyk/src/main/java/by/itAcademy/homeworks/basics/Task2.java
+++ Dzmitry Vasilchyk/src/main/java/by/itAcademy/homeworks/basics/Task2.java
@@ -1,4 +1,12 @@
 package by.itAcademy.homeworks.basics;
 
+/**Задание02:
+ вывести на экран “Hello Belarus!
+ **/
+
 public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("“Hello Belarus!");
+    }
 }
+
