f0da5a2608a79de5dd59733a033833d5e44bfb24
Meyhe <89459095+Meyhe@users.noreply.github.com>
2021-11-17 09:18:32 +0300

    Update Task3.java

--- Alina Artiomenko/src/main/java/by/itAcademy/homeworks/types/Task3.java
+++ Alina Artiomenko/src/main/java/by/itAcademy/homeworks/types/Task3.java
@@ -1,4 +1,25 @@
 package by.itAcademy.homeworks.types;
 
+/** Задание 04
+* Вывести в консоль значения заданных переменных
+**/
+
 public class Task3 {
+  byte b = 0x55;
+  short s = 0x55ff;
+  int i = 1000000;
+  long l = 0xffffffffL;
+  char c = 'a';
+  float f = .025f;
+  double = .00001234;
+  boolean bool = true;
+  
+  System.out.println("byte b = " + b);
+  System.out.println("short s = " + s);
+  System.out.println("int i = " + i);
+  System.out.println("long l = " + l);
+  System.out.println("char c = " + c);
+  System.out.println("float f = " + f);
+  System.out.println("double d = " + d);
+  System.out.println("boolean bool = " + bool);
 }
