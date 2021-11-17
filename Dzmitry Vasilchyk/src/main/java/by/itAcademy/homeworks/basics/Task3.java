4bf10c853d5aa6e8560951c1755544ffd345cb08
Dzmitry1405 <vdn_13@mail.ru>
2021-11-17 16:35:47 +0300

    Task3.java

--- Dzmitry Vasilchyk/src/main/java/by/itAcademy/homeworks/basics/Task3.java
+++ Dzmitry Vasilchyk/src/main/java/by/itAcademy/homeworks/basics/Task3.java
@@ -1,4 +1,12 @@
 package by.itAcademy.homeworks.basics;
 
+/**Задание03:
+ вывести на экран Hello Belarus! и I like Java!
+ **/
+
 public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Hello Belarus!");
+        System.out.println("I like Java!");
+    }
 }
