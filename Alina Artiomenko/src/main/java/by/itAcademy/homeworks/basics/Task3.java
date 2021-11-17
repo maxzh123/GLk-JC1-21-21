f3aac3fe42d0918b66f78408b984db3007acec51
Meyhe <89459095+Meyhe@users.noreply.github.com>
2021-11-17 09:12:24 +0300

    Update Task3.java

--- Alina Artiomenko/src/main/java/by/itAcademy/homeworks/basics/Task3.java
+++ Alina Artiomenko/src/main/java/by/itAcademy/homeworks/basics/Task3.java
@@ -1,4 +1,11 @@
 package by.itAcademy.homeworks.basics;
 
+/** Задание 03
+* Вывести в консоль "Hello Belarus!"  и "I like Java!"
+**/
+
 public class Task3 {
+  public static void main(String[] args){
+    System.out.println("Hello Belarus!" + "\nI like Java!");
+  }
 }
