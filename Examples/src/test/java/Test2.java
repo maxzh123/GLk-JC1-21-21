72aabc0e8b13208c3b661631a970fe0a6df83ddb
Siarhei <fyu92291@zwoho.com>
2021-11-18 19:04:46 +0300

    ghbvth

---
+++ Examples/src/test/java/Test2.java
@@ -0,0 +1,11 @@
+import by.itAcademy.homeworks.basics.Task1;
+import org.junit.jupiter.api.Test;
+
+public class Test2 {
+
+    @Test
+    public void test1(){
+        Task1.main(null);
+    }
+
+}
