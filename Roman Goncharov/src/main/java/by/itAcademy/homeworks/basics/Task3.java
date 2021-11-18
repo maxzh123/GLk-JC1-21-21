65392b40c1a5195b569a9d96c3cccb48374dca08
justify17 <roma.goncharov.1997@mail.ru>
2021-11-18 16:14:29 +0300

    qwe

--- Roman Goncharov/src/main/java/by/itAcademy/homeworks/basics/Task3.java
+++ Roman Goncharov/src/main/java/by/itAcademy/homeworks/basics/Task3.java
@@ -2,7 +2,7 @@ package by.itAcademy.homeworks.basics;
 
 public class Task3 {
   public static void main(String[] args) {
-        System.out.println("Hello Belarus!");
+        System.out.print("Hello Belarus!");
         System.out.println("I Like Java!");
     }
 }
