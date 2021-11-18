554c4f47ec08ac8f6b5515aa315dd3f729c0b235
justify17 <roma.goncharov.1997@mail.ru>
2021-11-18 16:34:42 +0300

    qwe

--- Roman Goncharov/src/main/java/by/itAcademy/homeworks/types/Task4.java
+++ Roman Goncharov/src/main/java/by/itAcademy/homeworks/types/Task4.java
@@ -1,4 +1,9 @@
 package by.itAcademy.homeworks.types;
 
 public class Task4 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a + b + a * b);
+    }
 }
