4d9f0e44c2b0e4f4812e77be8d3d8935b8b2535b
Sergey Khomiakov <tnkmaze4.0@gmail.com>
2021-11-19 16:47:34 +0300

    Completed "operators" (Task 6 - Task 11) P.S. Without UnitTest

--- Sergey Khomiakov/src/main/java/by/itAcademy/homeworks/operators/Task11.java
+++ Sergey Khomiakov/src/main/java/by/itAcademy/homeworks/operators/Task11.java
@@ -1,4 +1,61 @@
 package by.itAcademy.homeworks.operators;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+> Написать метод, который выводит расписание на неделю. Задать на вход в
+> метод порядковый номер дня недели и отобразить на экране то, что запланировано
+> на этот день.
+ */
+
 public class Task11 {
+    static int a;
+
+    public static void main(String[] args) {
+
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Chose day of week and write his number. You can use only whole number");
+            a = Integer.parseInt(reader.readLine());
+            getShedulle(a);
+        }
+        catch (Exception ex){
+            System.out.println("You have to write only whole number!");
+        }
+
+    }
+
+    private static void getShedulle(int dayZ){
+        switch(dayZ) {
+            case 1:
+                System.out.println("Today is Monday. You have to do your homework and go to the course");
+                break;
+            case 2:
+                System.out.println("Today is Tuesday. You have to do your homework and go to another course");
+                break;
+            case 3:
+                System.out.println("Today is Wednesday. You can relax today. It`s no bad for you! Keep the smile :) ");
+                break;
+            case 4:
+                System.out.println("Today is Thursday. You have to do your homework and go to another course ");
+                break;
+            case 5:
+                System.out.println("Today is Friday. You have to do your homework and can go for a walk ");
+                break;
+            case 6:
+                System.out.println("Today is Saturday. Just relax, because you are the BEST ");
+                break;
+            case 7:
+                System.out.println("Today is Sunday. Let`s go to the cinema!");
+                break;
+            default:
+                System.out.println("What`s the number?! Are you drunk? You need only choose numbers from 1 until 8!");
+                break;
+
+        }
+
+
+    }
+
 }
--- Sergey Khomiakov/src/main/java/by/itAcademy/homeworks/operators/Task9.java
+++ Sergey Khomiakov/src/main/java/by/itAcademy/homeworks/operators/Task9.java
@@ -1,4 +1,116 @@
 package by.itAcademy.homeworks.operators;
+/*
+> Задать три числа – день, месяц, год. Вывести на экран в виде трех чисел дату
+> следующего дня.
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 
 public class Task9 {
-}
+
+    static boolean  leapYear;
+    static int dayX,  monthX, yearX;
+
+    public static void main(String[] args)  {
+
+        System.out.println("Input your Date in according to next rule: dd mm yyyy");
+
+        try { BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            dayX = Integer.parseInt(reader.readLine());
+            monthX = Integer.parseInt(reader.readLine());
+            yearX = Integer.parseInt(reader.readLine());
+            System.out.println("Your Date is " + dayX + " " + monthX + " " + yearX);
+        } catch (Exception ex){
+            System.out.println("You input incorrect Date");
+
+        }
+
+
+
+        leapYear=isLeapYear(yearX);
+        getDate(dayX,  monthX, yearX);
+
+    }
+
+    private static void getDate(int day, int month, int year){
+
+
+                if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10){
+
+                    if(day>0 && day<=30){
+                        day++;
+                        System.out.println("Next day is... " + day + " " + month + " " + year);
+                    } else if (day==31) {
+                        day = 1;
+                        month++;
+                        System.out.println();
+                        System.out.println(day + " " + month + " " + year);
+                    } else {
+                        System.out.println("Please, use Date in according to Gregorian Calendar");
+                    }
+
+                } else if(month == 2 ){
+                    if(day>0 && day <= 27 ){
+                        day++;
+                        System.out.println("Next day is... " + day + " " + month + " " + year);
+                    } else if (day==28 &&leapYear) {
+                        day++;
+                        System.out.println("Next day is... " + day + " " + month + " " + year);
+                    } else if (day==28 ) {
+                        day=1;
+                        month++;
+                        System.out.println("Next day is... " + day + " " + month + " " + year);
+                    }
+                    else if(day==29 && leapYear){
+                        day = 1;
+                        month++;
+                        System.out.println("Next day is... " + day + " " + month + " " + year);
+                    } else {
+                        System.out.println("Please, use Date in according to Gregorian Calendar");
+                    }
+
+                } else if(month == 4 || month == 6 || month == 9 || month == 11) {
+                    if (day>0 && day <= 29) {
+                        day++;
+                        System.out.println("Next day is... " + day + " " + month + " " + year);
+                    } else if (day == 30) {
+                        day = 1;
+                        month++;
+                        System.out.println("Next day is... " + day + " " + month + " " + year);
+                    } else {
+                        System.out.println("Please, use Date in according to Gregorian Calendar");
+                    }
+                } else if(month == 12){
+                    if (day>0 && day <= 30) {
+                        day++;
+                        System.out.println("Next day is... " + day + " " + month + " " + year);
+                    } else if (day == 31) {
+                        day = 1;
+                        month = 1;
+                        year++;
+                        System.out.println("Next day is... " + day + " " + month + " " + year);
+                }
+
+            }else{  System.out.println("Please, use Date in according to Gregorian Calendar");
+
+            }
+
+    }
+
+        private static boolean isLeapYear(int yearCh){
+            if(yearCh%400==0){
+                return true;}
+
+                else if (yearCh%100==0){
+                    return false;
+                }
+                else if (yearCh%4==0){
+                return true;
+                }
+                else{
+                    return  false;
+            }
+        }
+    }
