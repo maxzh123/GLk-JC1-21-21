c2db30f9c2e08f972719908e905cc87f40566f7d
EkaterinaCherkas <94483993+EkaterinaCherkas@users.noreply.github.com>
2021-11-20 13:37:32 +0300

    Update Task4.java

--- Ekaterina Cherkas/src/main/java/by/itAcademy/homeworks/types/Task4.java
+++ Ekaterina Cherkas/src/main/java/by/itAcademy/homeworks/types/Task4.java
@@ -1,4 +1,15 @@
 package by.itAcademy.homeworks.types;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Task4 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n1 = Integer.parseInt(reader.readLine());
+        int n2 = Integer.parseInt(reader.readLine());
+        int sum = n1 + n2;
+        int result = sum + n1*n2;
+        System.out.println(result);
+    }
 }
