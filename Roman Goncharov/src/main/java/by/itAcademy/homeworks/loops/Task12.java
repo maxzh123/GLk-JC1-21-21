dd2b39d49373ef9458188c9e4e2fc9b53afd8155
Roma <roma.goncharov.1997@mail.ru>
2021-11-21 21:10:48 +0300

    Updated

--- Roman Goncharov/src/main/java/by/itAcademy/homeworks/loops/Task12.java
+++ Roman Goncharov/src/main/java/by/itAcademy/homeworks/loops/Task12.java
@@ -1,4 +1,14 @@
 package by.itAcademy.homeworks.loops;
 
 public class Task12 {
+    public static void main(String[] args) {
+        System.out.println("0! = 1");
+        int result = 1; // Факториал 0 - равен 1. Это правило.
+        int i = 1; // Т.к. факториал 0 мы уже знаем из правила наше первое число в цикле - 1.
+        while (i <= 10) {
+           result = result * i;
+           System.out.println(i + "! = " + result);
+           i++;
+        }
+    }
 }
