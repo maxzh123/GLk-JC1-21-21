97ed8bb051c9f2af80e14061af19534e4049df92
Vlad Avchinnikov <v.avchinnikov@mail.ru>
2021-11-22 01:12:16 +0300

    первые 5 заданий

--- Vlad Avchinnikov/src/main/java/by/itAcademy/homeworks/types/Task4.java
+++ Vlad Avchinnikov/src/main/java/by/itAcademy/homeworks/types/Task4.java
@@ -1,4 +1,18 @@
 package by.itAcademy.homeworks.types;
 
+import java.util.Scanner;
+
 public class Task4 {
+
+    public static int getMath(int a, int b) {
+        return (a + b) + (a * b);
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        System.out.println("Результат = " + getMath(a, b));
+
+    }
 }
