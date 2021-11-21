a0c18a52de2cd8904cffb0ab793164c16613e6fa
NickolaiDemidovich <Nicknewb97@gmail.com>
2021-11-21 22:32:22 +0300

    complete task8,11

--- Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/operators/Task11.java
+++ Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/operators/Task11.java
@@ -1,4 +1,46 @@
 package by.itAcademy.homeworks.operators;
 
+import java.util.Scanner;
+
 public class Task11 {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Какой день недели Вас интересует ?");
+        int weekNubmer = scr.nextInt();
+        switch (weekNubmer){
+            case 1:
+                System.out.println("Понедельник");
+                System.out.println("Встреча в 15 30");
+                break;
+            case 2:
+                System.out.println("Вторник");
+                System.out.println("Ничего не запланированно");
+                break;
+            case 3:
+                System.out.println("Среда");
+                System.out.println("Курсы в 19 30");
+                break;
+            case 4:
+                System.out.println("Четверг");
+                System.out.println("Тренировка в 18 00");
+                break;
+            case 5:
+                System.out.println("Пятница");
+                System.out.println("Встреча в баре");
+                break;
+            case 6:
+                System.out.println("Суббота");
+                System.out.println("Встерча с родственниками");
+                break;
+            case 7:
+                System.out.println("Воскресенье");
+                System.out.println("День рождения у Евгения");
+                break;
+            default:
+                System.out.println("День недели не найден");
+                break;
+
+
+        }
+    }
 }
