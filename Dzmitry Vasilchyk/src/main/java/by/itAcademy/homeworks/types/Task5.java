1b2bf5df0dee9fdcb9ea7c11c1d9d0c8a01de6da
Dzmitry1405 <vdn_13@mail.ru>
2021-11-21 18:05:22 +0300

    Task5.java повторно

--- Dzmitry Vasilchyk/src/main/java/by/itAcademy/homeworks/types/Task5.java
+++ Dzmitry Vasilchyk/src/main/java/by/itAcademy/homeworks/types/Task5.java
@@ -1,14 +1,19 @@
 package by.itAcademy.homeworks.types;
 
-
+import java.util.Scanner;
 /**Задание05:
  заданный промежуток времени в секундах
  вывести на экран в виде часов, минут, секунд, суток и недель
- **/
+ */
 
 public class Task5 {
     public static void main(String[] args) {
-        int x = 1000000;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите количество секунд:");
+        int x = in.nextInt();
+        countSek(x);
+    }
+        private static void countSek(int x) {
         int s = x % 60;
         int min = x / 60;
         int m = min % 60;
@@ -17,9 +22,6 @@ public class Task5 {
         int day = hour / 24;
         int d = day % 7;
         int w = day / 7;
-
-
-        System.out.println(s + " second" + "\n" + m + " minutes" + "\n" + h + " hour" + "\n"
-                + d + " day" + "\n" + w + " week");
+        System.out.println(x + " секунд составит:" + "\n" + s + " second" + "\n" + m + " minutes" + "\n" + h + " hour" + "\n"
+                + d + " day" + "\n" + w + " week");}
     }
-}
