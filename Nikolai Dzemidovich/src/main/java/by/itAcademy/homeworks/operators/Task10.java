0edd68555689b61d816cd3ad04e30eccb63ec721
NickolaiDemidovich <Nicknewb97@gmail.com>
2021-11-22 13:52:27 +0300

    complete task9,10

--- Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/operators/Task10.java
+++ Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/operators/Task10.java
@@ -1,4 +1,25 @@
 package by.itAcademy.homeworks.operators;
 
+import java.util.Scanner;
+
 public class Task10 {
+    public static void main(String[] args) {
+
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Введите стороны первого дома");
+        int a = scr.nextInt(), b = scr.nextInt();
+        System.out.println("Введите стороны второго дома");
+        int c = scr.nextInt(), d = scr.nextInt();
+        System.out.println("Введите стороны участка");
+        int e = scr.nextInt(), f = scr.nextInt();
+        if(Math.max(a,b) <= Math.max(e,f) && Math.max(c,d) <= Math.max(e,f) && Math.min(a,b) <= Math.min(e,f) && Math.min(c,d) <= Math.min(e,f)){
+            if((e*f) >= (a*b) + (c*d)){
+                System.out.println("Дома помещаются на участке");
+            } else {
+                System.out.println("Дома не помещаются на учатске");
+            }
+        } else {
+            System.out.println("Дома не помещаются на учатске");
+        }
+    }
 }
