24bd9a112bfb83f7c409ef2660c6f0f9e8cbad29
Siarhei <fyu92291@zwoho.com>
2021-11-22 18:30:30 +0300

    переименование

--- Examples/src/main/java/by/itAcademy/homeworks/basics/Task2.java
+++ Examples/src/main/java/by/itAcademy/homeworks/basics/Task2.java
@@ -1,4 +1,25 @@
 package by.itAcademy.homeworks.basics;
 
 public class Task2 {
+    public static void main(String[] args) {
+        Integer x=8;
+        Integer y=050;
+        System.out.println(y);
+
+
+
+//        if (getFalse()&getTrue()){
+//
+//        }
+    }
+
+    static boolean getTrue(){
+        System.out.println("getTrue");
+        return  true;
+    }
+    static boolean getFalse(){
+        System.out.println("getFalse");
+        return  false;
+    }
+
 }
