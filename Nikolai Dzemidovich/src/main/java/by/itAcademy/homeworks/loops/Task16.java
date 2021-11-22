7be83b457b75b01777c994d9fe989a9fdc9c1900
NickolaiDemidovich <Nicknewb97@gmail.com>
2021-11-22 22:07:47 +0300

    Compete task 12,13,16

--- Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/loops/Task16.java
+++ Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/loops/Task16.java
@@ -1,4 +1,9 @@
 package by.itAcademy.homeworks.loops;
 
 public class Task16 {
+    public static void main(String[] args) {
+        for (int a = 1; a < 101; a++) {
+            if (a % 7 == 0) System.out.println("Hope, number is " + a);
+        }
+    }
 }
