c809493e61fa58b6913af8a10f24d92e3a3bf3a5
EkaterinaCherkas <94483993+EkaterinaCherkas@users.noreply.github.com>
2021-11-22 14:42:27 +0300

    Update Task7.java

--- Ekaterina Cherkas/src/main/java/by/itAcademy/homeworks/operators/Task7.java
+++ Ekaterina Cherkas/src/main/java/by/itAcademy/homeworks/operators/Task7.java
@@ -1,4 +1,15 @@
 package by.itAcademy.homeworks.operators;
 
 public class Task7 {
-}
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 16;
+        int r = 10;
+
+        if (r*r >= (a*a + b*b)/4){
+            System.out.println("Можно");
+        } else{
+            System.out.println("Нельзя");}
+        }
+
+    }
