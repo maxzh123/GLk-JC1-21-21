2e9ed9dcb4ee6e559301374d0257fdec6213597c
alinaflaynger <alinaflaynger@gmail.com>
2021-11-22 21:13:45 +0300

    Update Task2.java

--- Alina Shevlyukova/src/main/java/by/itAcademy/homeworks/types/Task5.java
+++ Alina Shevlyukova/src/main/java/by/itAcademy/homeworks/types/Task5.java
@@ -1,4 +1,12 @@
 package by.itAcademy.homeworks.types;
 
+import java.util.Scanner;
+
 public class Task5 {
+    public static void main(String[] args) {
+        System.out.println("введите кол-во сек:");
+        Scanner inputFigure = new Scanner(System.in);
+        int i = inputFigure.nextInt();
+        System.out.println("Минут:" + i/60+"\n"+"Часов:" + i/3600 + "\n" +"Дней:"+ i/86400 +"\n"+"Недель:"+ i/604800 );
+    }
 }
