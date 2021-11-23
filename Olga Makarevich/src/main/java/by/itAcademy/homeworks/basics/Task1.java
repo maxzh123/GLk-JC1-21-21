b0c1641cc9e2b809a429300563bba935cae5f7d2
Olga Makarevich <olgagomel17@mail.ru>
2021-11-23 22:21:09 +0300

    comment

--- Olga Makarevich/src/main/java/by/itAcademy/homeworks/basics/Task1.java
+++ Olga Makarevich/src/main/java/by/itAcademy/homeworks/basics/Task1.java
@@ -1,5 +1,7 @@
 package by.itAcademy.homeworks.basics;
 
+// Набрать приведенный пример HelloWorld.java, откомпилировать его и запустить.
+
 public class Task1 {
   public static void main (String[] args) {
   System.out.println("Hello world!");
