78ab13a5b301571be61c55ac58b459e5d6720813
Roma <roma.goncharov.1997@mail.ru>
2021-11-24 23:10:50 +0300

    Complete Task 15

--- Roman Goncharov/src/main/java/by/itAcademy/homeworks/loops/Task15.java
+++ Roman Goncharov/src/main/java/by/itAcademy/homeworks/loops/Task15.java
@@ -1,4 +1,23 @@
 package by.itAcademy.homeworks.loops;
 
+// Задание 15
+// Найти среди чисел от 50 до 70 второе простое число и вывести его на экран.
+
 public class Task15 {
+    public static void main(String[] args) {
+        int a = 0;
+            for (int i = 50; i<=70; i++) {
+                int j = 2;
+                while (i%j != 0) {
+                    j++;
+                }
+                if (j == i) {
+                    if (i > a && a != 0) {
+                        System.out.println(i);
+                        break;
+                    }
+                    a = j;
+                }
+            }
+    }
 }
