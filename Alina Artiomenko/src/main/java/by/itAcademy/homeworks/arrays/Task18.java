8a33ddc24bf3f91959e31507d035b439ec450dcf
Alina Artiomenko <meyhe@example.com>
2021-11-25 01:00:36 +0300

    Tasks 18-19

--- Alina Artiomenko/src/main/java/by/itAcademy/homeworks/arrays/Task18.java
+++ Alina Artiomenko/src/main/java/by/itAcademy/homeworks/arrays/Task18.java
@@ -1,4 +1,19 @@
 package by.itAcademy.homeworks.arrays;
 
+/**Задание 18
+ * 1. Создайть массив из 10 элементов.
+ * 2. Заполнить произвольными значениями целочисленного типа
+ * 3. Вывести последний элемент массива
+ */
+
 public class Task18 {
+    public static void main(String[] args){
+        int[] arrayFirst = new int[10];
+
+        for (int i = 0; i < 10; i++){
+            arrayFirst[i] = (int) (10 * Math.random());
+        }
+
+        System.out.println(arrayFirst[9]);
+    }
 }
--- Alina Artiomenko/src/main/java/by/itAcademy/homeworks/arrays/Task19.java
+++ Alina Artiomenko/src/main/java/by/itAcademy/homeworks/arrays/Task19.java
@@ -1,4 +1,17 @@
 package by.itAcademy.homeworks.arrays;
 
+/**Задание 19
+ * 1. Создать массив из 10 элементов
+ * 2. Заполнить массив произвольными значениями типа int
+ * 3. Выведите на экран элементы, стоящие на четных позициях
+ */
 public class Task19 {
+    public static void main(String[] args){
+        int [] arraySecond = {15, -3, 14, 0, 33, 4, -45, -78, 8, 3};
+
+        for (int i = 0; i < 10; i++){
+            if (i % 2 == 0 || i == 0)
+                System.out.println("arr[" + i + "] = " + arraySecond[i]);
+        }
+    }
 }
