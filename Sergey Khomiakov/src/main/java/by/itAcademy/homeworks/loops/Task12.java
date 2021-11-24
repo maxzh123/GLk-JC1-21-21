c0051c09f886edfcd69b8633ed82666d30d3d2ac
Sergey Khomiakov <tnkmaze4.0@gmail.com>
2021-11-24 16:16:28 +0300

    Completed Task 12-14,16,17 "loops"

--- Sergey Khomiakov/src/main/java/by/itAcademy/homeworks/loops/Task12.java
+++ Sergey Khomiakov/src/main/java/by/itAcademy/homeworks/loops/Task12.java
@@ -1,4 +1,18 @@
 package by.itAcademy.homeworks.loops;
+/*
+Вычислить факториал целых чисел от 0 до 10 с помощью цикла while
+ */
 
 public class Task12 {
+
+    public int getFactorialFrom0to10(){
+        int factorial = 1;
+        int count = 10;
+
+        while (count > 0){
+            factorial*=count;
+            count--;
+        }
+        return factorial;
+    }
 }
