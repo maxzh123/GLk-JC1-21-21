f3c426eadea232771bc82d02302522c73cec8683
Alina Artiomenko <meyhe@example.com>
2021-11-24 23:53:23 +0300

    Tasks 6-8

--- Alina Artiomenko/src/main/java/by/itAcademy/homeworks/operators/Task6.java
+++ Alina Artiomenko/src/main/java/by/itAcademy/homeworks/operators/Task6.java
@@ -1,4 +1,32 @@
 package by.itAcademy.homeworks.operators;
 
+/** Задание 06
+ * 1. Ввод целочисленного параметра
+ * 2. Является ли последняя цифра числа семёркой
+ * 3. Вернуть true или false
+ */
+
+import java.io.*;
+
 public class Task6 {
+    public static void main(String[] args){
+        BufferedReader inputNum = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Введите целочисленной число: ");
+        try {
+            int num = Integer.parseInt(inputNum.readLine());
+            System.out.print(lastDigitSeven(num));
+        } catch (Exception ex){
+            System.out.print("Некоректный вовод!");
+        }
+    }
+
+    public static boolean lastDigitSeven(int x){
+        if (x % 10 == 7){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+
+
