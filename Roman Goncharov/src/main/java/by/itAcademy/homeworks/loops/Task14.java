81016a842fc0ddd6037fd19020ad57ef947e1101
Roma <roma.goncharov.1997@mail.ru>
2021-11-24 13:04:30 +0300

    Updated

--- Roman Goncharov/src/main/java/by/itAcademy/homeworks/loops/Task14.java
+++ Roman Goncharov/src/main/java/by/itAcademy/homeworks/loops/Task14.java
@@ -1,4 +1,17 @@
 package by.itAcademy.homeworks.loops;
 
+// Задание №14
+// Посчитать сумму цифр заданного числа.
+
 public class Task14 {
+    public static void main(String[] args) {
+        long num = 7893823445L;
+        long sum = 0;
+        do {
+            long a = num % 10;
+            num = num / 10;
+            sum = sum + a;
+        } while (num != 0);
+        System.out.println("Сумма цифр заданного числа равна " + sum);
+    }
 }
