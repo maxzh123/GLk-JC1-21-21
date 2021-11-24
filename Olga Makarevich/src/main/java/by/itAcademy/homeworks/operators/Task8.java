df0a8eb4103f80d055d1dfb3685c0b5969788691
Olga Makarevich <olgagomel17@mail.ru>
2021-11-24 22:46:33 +0300

    Task8

--- Olga Makarevich/src/main/java/by/itAcademy/homeworks/operators/Task8.java
+++ Olga Makarevich/src/main/java/by/itAcademy/homeworks/operators/Task8.java
@@ -1,4 +1,21 @@
 package by.itAcademy.homeworks.operators;
 
+// Задать целое число в виде переменной, это число – сумма денег в рублях.
+// Вывести это число на экран, добавив к нему слово «рублей» в правильном падеже
+
 public class Task8 {
+    public static void main(String[] args) {
+        int a;
+        a = (int) (Math.random() * 100000);
+        System.out.println(a);
+        if (a % 10 == 1) {
+            System.out.println(" рубль");
+        } else if (a % 10 == 2 | a % 10 == 3 | a % 10 == 4) {
+            System.out.println(" рубля");
+        } else if (a % 10 == 5 | a % 10 == 6 | a % 10 == 7 | a % 10 == 8 | a % 10 == 9 | a % 10 == 0) {
+            System.out.println(" рублей");
+        }
+    }
 }
+
+// переделать с методом
