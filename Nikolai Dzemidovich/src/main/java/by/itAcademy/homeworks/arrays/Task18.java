fd34b144f9ddcb6c8b261455ff440986a75408fa
NickolaiDemidovich <Nicknewb97@gmail.com>
2021-11-24 22:42:21 +0300

    Complete Task 13-15, 18-19

--- Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/arrays/Task18.java
+++ Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/arrays/Task18.java
@@ -1,4 +1,25 @@
 package by.itAcademy.homeworks.arrays;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task18 {
+    public static void main(String[] args) {
+
+        int[] sca = Task18.randomArrays(10);
+        System.out.println(Arrays.toString(sca));
+
+    }
+
+    public static int[] randomArrays(int a) {
+        int[] array = new int[a];
+
+        Random rand = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(199);
+        }
+
+        return array;
+    }
 }
--- Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/loops/Task14.java
+++ Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/loops/Task14.java
@@ -1,4 +1,22 @@
 package by.itAcademy.homeworks.loops;
 
 public class Task14 {
+    public static void main(String[] args) {
+
+        System.out.println("Сумма = " + Task14.getTheSumOfNumbers(7893823445L));
+
+    }
+
+    public static int getTheSumOfNumbers(long number) {
+
+        int sum = 0;
+
+        do {
+            sum += number % 10;
+            number /= 10;
+        } while (number != 0);
+
+        return sum;
+    }
 }
+
