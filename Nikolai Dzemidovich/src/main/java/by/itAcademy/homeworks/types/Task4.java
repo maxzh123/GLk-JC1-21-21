c48f6dac7f19253f7d1ddf696786de498bf6921a
NickolaiDemidovich <Nicknewb97@gmail.com>
2021-11-24 19:51:27 +0300

    Update Task4 \ add MyReadHelper class

--- Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/types/Task4.java
+++ Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/types/Task4.java
@@ -1,21 +1,14 @@
 package by.itAcademy.homeworks.types;
 //Написать метод, который принимает на вход два целых числа, делает их суммирование и складывает результат с произведением двух этих чисел, и возвращает полученный результат из метода. Передать на вход в метод любые два числа. Вывести полученный результат работы метода на экран
-import java.util.Scanner;
 
 public class Task4 {
-  
-  public static void main(String[] args) {
 
-        Scanner scr = new Scanner(System.in);
+    public static void main(String[] args) {
 
-        System.out.println("Введите первое число");
-        int firstNumber = scr.nextInt();
+        int firstNumber = MyReadHelper.readInt("первое число");
+        int secondNumber = MyReadHelper.readInt("второе число");
 
-        System.out.println("Введите второе число");
-        int secondNumber = scr.nextInt();
-
-        System.out.println((firstNumber+secondNumber) + (firstNumber*secondNumber));
-
-
+        System.out.println((firstNumber + secondNumber) + (firstNumber * secondNumber));
     }
 }
+
