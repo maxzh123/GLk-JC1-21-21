4f8302f0e7d285bfc6acb6f8601a0df71e92208e
alinaflaynger <alinaflaynger@gmail.com>
2021-11-24 10:12:22 +0300

    Update Task2.java

--- Alina Shevlyukova/src/main/java/by/itAcademy/homeworks/operators/Task8.java
+++ Alina Shevlyukova/src/main/java/by/itAcademy/homeworks/operators/Task8.java
@@ -1,4 +1,16 @@
 package by.itAcademy.homeworks.operators;
 
+import java.util.Scanner;
+
 public class Task8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        double a= input.nextDouble();
+    if (a==1) {
+        System.out.println(a + "рубль");
+    }else{
+        System.out.println(a+"рублей");
+    }
+    }
 }
