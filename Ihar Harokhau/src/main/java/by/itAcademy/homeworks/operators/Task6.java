11a81feedb5123b88a829f79655be5d93c0c60b6
Ihar <ihar_harokhau@example.com>
2021-11-25 15:34:37 +0300

    update task 6

--- Ihar Harokhau/src/main/java/by/itAcademy/homeworks/operators/Task6.java
+++ Ihar Harokhau/src/main/java/by/itAcademy/homeworks/operators/Task6.java
@@ -9,6 +9,7 @@ import java.io.InputStreamReader;
  *
  * Создайте метод с одним целочисленным параметром.
  * Метод должен определить, является ли последняя цифра числа семеркой и вернуть boolean значение.
+ *
  */
 public class Task6 {
     public static void main(String[] args) throws IOException {
