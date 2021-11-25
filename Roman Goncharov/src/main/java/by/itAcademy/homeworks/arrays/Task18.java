2f541367c1c7d5e985dadd82e28c7f495d1bbbd2
Roma <roma.goncharov.1997@mail.ru>
2021-11-26 01:00:38 +0300

    Updated Task16. Complete Task18, Task19

--- Roman Goncharov/src/main/java/by/itAcademy/homeworks/arrays/Task18.java
+++ Roman Goncharov/src/main/java/by/itAcademy/homeworks/arrays/Task18.java
@@ -1,16 +1,21 @@
 package by.itAcademy.homeworks.arrays;
 
+// Задание 18
+// Создать массив из 10 элементов, заполнить его произвольными целочисленными значениями и
+// вывести последний элемент массива на экран.
+
 public class Task18 {
     public static void main(String[] args) {
         int[] array = new int[10];
-        System.out.println(last());
+        fillingTheArray(array);
+        System.out.println(array[array.length - 1]);
     }
 
-    public static int[] last(int[] array) {
-        int[] a = array;
-        for (int i = 0; i < a.length; i = i++) {
-            a[i] = i + 2;
+    public static int[] fillingTheArray(int[] a) {
+        int[] newArray = a;
+        for (int i = 0; i < newArray.length; i++) {
+            newArray[i] = i*2;
         }
-        return a;
+        return newArray;
     }
 }
--- Roman Goncharov/src/main/java/by/itAcademy/homeworks/arrays/Task19.java
+++ Roman Goncharov/src/main/java/by/itAcademy/homeworks/arrays/Task19.java
@@ -1,4 +1,15 @@
 package by.itAcademy.homeworks.arrays;
 
+// Задание 18
+// Создать массив из 10 элементов, заполнить его произвольными целочисленными значениями и
+// вывести элементы, стоящие на четных позициях.
+
 public class Task19 {
+    public static void main(String[] args) {
+        int array [] = new int[10];
+        Task18.fillingTheArray(array);
+        for (int i = 1; i < array.length; i = i + 2) { // i = 1 если нужно вывести элементы на четных позициях
+            System.out.println(array[i]);              // i = 0 если нужно вывести элементы с четными индексами
+        }
+    }
 }
