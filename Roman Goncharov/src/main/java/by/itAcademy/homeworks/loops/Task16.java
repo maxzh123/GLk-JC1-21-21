5cd05ba465ea4e454363f36aa836cf573bb3b3a6
Roma <roma.goncharov.1997@mail.ru>
2021-11-25 17:17:41 +0300

    Complete Task16

--- Roman Goncharov/src/main/java/by/itAcademy/homeworks/loops/Task16.java
+++ Roman Goncharov/src/main/java/by/itAcademy/homeworks/loops/Task16.java
@@ -1,4 +1,11 @@
 package by.itAcademy.homeworks.loops;
 
 public class Task16 {
+    public static void main(String[] args) {
+        for (int i = 1; i<=100; i++) {
+            if (i%7 == 0) {
+                System.out.println(i + " Hope!");
+            }
+        }
+    }
 }
