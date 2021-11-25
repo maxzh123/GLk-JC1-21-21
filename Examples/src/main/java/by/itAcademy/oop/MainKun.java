0339252c0aa2232bb1a2b47eaedebe575f71d114
Siarhei <fyu92291@zwoho.com>
2021-11-25 19:25:17 +0300

    пример

--- Examples/src/main/java/by/itAcademy/oop/MainKun.java
+++ Examples/src/main/java/by/itAcademy/oop/MainKun.java
@@ -1,6 +1,15 @@
 package by.itAcademy.oop;
 
 public class MainKun extends Cat{
+
+    public MainKun(){
+        super("Мэйнкун");
+    }
+
+    public void setIam(String iam){
+        this.iam=iam;
+    }
+
     @Override
     public void say(){
         System.out.println("Я сожру вашего тузика!");
