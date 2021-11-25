184d6393d5d01acf8cd6dcd438656e2b249dd0b9
Roma <roma.goncharov.1997@mail.ru>
2021-11-25 22:43:17 +0300

    Не доделан Task18

--- Roman Goncharov/src/main/java/by/itAcademy/homeworks/arrays/Task18.java
+++ Roman Goncharov/src/main/java/by/itAcademy/homeworks/arrays/Task18.java
@@ -1,4 +1,16 @@
 package by.itAcademy.homeworks.arrays;
 
 public class Task18 {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        System.out.println(last());
+    }
+
+    public static int[] last(int[] array) {
+        int[] a = array;
+        for (int i = 0; i < a.length; i = i++) {
+            a[i] = i + 2;
+        }
+        return a;
+    }
 }
