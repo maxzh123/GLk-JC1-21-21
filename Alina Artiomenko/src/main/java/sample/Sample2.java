29ed85f8b57b129530417bb6d82301b731952b78
Maksim S Zhalezny <M.Geleznu@novacom.by>
2021-11-26 09:39:19 +0300

    правка репозитория

---
+++ Alina Artiomenko/src/main/java/sample/Sample2.java
@@ -0,0 +1,13 @@
+package sample;
+/* Задание:
+1. запустить убедится, что работает.
+2. изменить количество повторений.
+* */
+
+public class Sample2 {
+    public static void main (String[] arg){
+        for (int i = 0; i <= 25; i++){
+            System.out.println("Повторение №" + i);
+        }
+    }
+}
