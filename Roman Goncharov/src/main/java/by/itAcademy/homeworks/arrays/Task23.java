8126f178e42fd54f4fc6fa41978445a140786802
Roma <roma.goncharov.1997@mail.ru>
2021-11-27 18:01:39 +0300

    Completed Task 22, Task 23

--- Roman Goncharov/src/main/java/by/itAcademy/homeworks/arrays/Task23.java
+++ Roman Goncharov/src/main/java/by/itAcademy/homeworks/arrays/Task23.java
@@ -1,4 +1,21 @@
 package by.itAcademy.homeworks.arrays;
 
+// Задание 23
+// Создать двумерный массив и заполнить его 'бабочкой'.
+
 public class Task23 {
+    public static void main(String[] args) {
+        int[][] array = new int[5][5];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if ((i == 1 || i == 3) && (j == 0 || j == 4) || i == 2 && j != 2) {
+                    System.out.print(array[i][j] + " ");
+                } else {
+                    array[i][j] = 1;
+                    System.out.print(array[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
