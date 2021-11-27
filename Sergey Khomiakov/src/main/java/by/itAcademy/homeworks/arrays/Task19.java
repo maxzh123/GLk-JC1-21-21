1812092e8abbf20173931f7587689dfe7a92903d
Sergey Khomiakov <tnkmaze4.0@gmail.com>
2021-11-27 16:18:21 +0300

    Completed Tasks 15,18,19,20. Added some fixes in Task 17.

--- Sergey Khomiakov/src/main/java/by/itAcademy/homeworks/arrays/Task19.java
+++ Sergey Khomiakov/src/main/java/by/itAcademy/homeworks/arrays/Task19.java
@@ -1,4 +1,16 @@
 package by.itAcademy.homeworks.arrays;
 
+/* Создайте переменную для массива из 10 элементов (другим способом).
+Заполните его произвольными значениями целочисленного типа и выведите на
+экран элементы, стоящие на четных позициях.*/
+
 public class Task19 {
+    public static void main(String[] args) {
+
+        int mySecondArray[] = Task18.getHelperToCreateArray(10);
+        for (int i = 1; i < mySecondArray.length; i = i + 2) {
+            System.out.println(mySecondArray[i]);
+        }
+
+    }
 }
--- Sergey Khomiakov/src/test/java/Task17Test.java
+++ Sergey Khomiakov/src/test/java/Task17Test.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class Task17Test {
     Task17 task17 = new Task17();
     @ParameterizedTest
-    @CsvSource({"-17099008,'-17 099 008'", "0, '0'", "20023143, '20 023 143'"})
-    void testGetTheSumOfUnits(int num, String result){
+    @CsvSource({"-17099008,'-17 099 008'", "0, '0'", "20023143, '20 023 143'", "45, '45'"})
+    void testGetNewFormatString(int num, String result){
         Assertions.assertEquals(task17.getNewFormatString(num),result);
     }
 }
