f1421d1100f8b39cf14b499de251ff4a7d6ec035
Olga Makarevich <olgagomel17@mail.ru>
2021-11-28 11:15:26 +0300

    Task7 done

--- Olga Makarevich/src/main/java/by/itAcademy/homeworks/operators/Task7.java
+++ Olga Makarevich/src/main/java/by/itAcademy/homeworks/operators/Task7.java
@@ -1,4 +1,17 @@
 package by.itAcademy.homeworks.operators;
 
+//Имеется прямоугольное отверстие размерами a и b, где a и b – целые числа.
+//Определить, можно ли его полностью закрыть круглой картонкой радиусом r (тоже целое число).
+
 public class Task7 {
+    public static void main(String[] args) {
+        int a = (int) (Math.random()*1000);
+        int b = (int) (Math.random()*1000);
+        int r = (int) (Math.random()*1000);
+        if (r * 2 >= Math.sqrt((Math.pow(a, 2)) + (Math.pow(b, 2)))) {
+            System.out.println("круглая картонка закроет отверстие");
+        } else {
+            System.out.println("круглая картонка не закроет отверстие");
+        }
+    }
 }
