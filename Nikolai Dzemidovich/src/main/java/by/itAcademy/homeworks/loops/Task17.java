955c96fd46b32cf98d48468d5275591ca731e2b1
NickolaiDemidovich <Nicknewb97@gmail.com>
2021-11-28 22:13:04 +0300

    Need help with task 17;

--- Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/loops/Task17.java
+++ Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/loops/Task17.java
@@ -1,4 +1,41 @@
 package by.itAcademy.homeworks.loops;
 
+import by.itAcademy.homeworks.types.MyReadHelper;
+
 public class Task17 {
+
+    /*У меня проблема. Если вводишь число, которое делиться без остатка на 3, то при выведении в бух формате, вначале приписывается ноль
+    Идея моя состоит в том, чтобы остатки от деления числа засунуть в массив, потом вывести его обратной последовательностью
+     */
+    public static void main(String[] args) {
+
+        long number = MyReadHelper.readInt("число");
+
+        int countOfDigits = getCountOfDigits(number);
+
+        int arrayLength = Math.round(countOfDigits / 3);
+
+        int[] array = new int[arrayLength + 1];
+
+        for (int i = 0; number != 0; i++) {
+            array[i] = (int) (number % 1000);
+            number /= 1000;
+            System.out.println(array[i]);
+        }
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static int getCountOfDigits(long number) {
+        int count = 0;
+        while (number != 0) {
+            number /= 10;
+            count++;
+        }
+        return count;
+    }
 }
+
+
