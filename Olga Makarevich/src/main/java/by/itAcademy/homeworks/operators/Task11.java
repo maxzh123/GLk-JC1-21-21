a1ed76632a798e9495b3e456ae0c85b4c95afd97
Olga Makarevich <olgagomel17@mail.ru>
2021-11-28 19:29:07 +0300

    Tack 11 done + Q

--- Olga Makarevich/src/main/java/by/itAcademy/homeworks/operators/Task11.java
+++ Olga Makarevich/src/main/java/by/itAcademy/homeworks/operators/Task11.java
@@ -4,45 +4,42 @@ package by.itAcademy.homeworks.operators;
 // Задать на вход в метод порядковый номер дня недели и отобразить на экране то, что запланировано на этот день.
 
 import java.util.Scanner;
-
 public class Task11 {
-    public static void main(String[] args) {
-        String Mon = "Gym";
-        String Tue = "Swimmingpool";
-        String Wed = "Tennis";
-        String Thu = "Basketball";
-        String Fri = "Soccer";
-        String Sat = "Chess";
-        String Sun = "Relax";
-        System.out.println(Mon);
-        System.out.println(Tue);
-        System.out.println(Wed);
-        System.out.println(Thu);
-        System.out.println(Fri);
-        System.out.println(Sat);
-        System.out.println(Sun);
-        Scanner timeTable = new Scanner(System.in);
-        System.out.println("Введите порядковый номер дня недели");
-        int numbDay = timeTable.nextInt();
-    }
+        private static String Mon = "Mon - Gym";
+        private static String Tue = "Tue - Swimmingpool";
+        private static String Wed = "Wed - Tennis";
+        private static String Thu = "Thu - Basketball";
+        private static String Fri = "Fri - Soccer";
+        private static String Sat = "Sat - Chess";
+        private static String Sun = "Sun - Relax";
 
-    public static int numbDay(int numbDay) {
-        if (numbDay == 1) {
-            System.out.println(Mon);
-        } else if (numbDay == 2) {
-            System.out.println(Tue);
-        } else if (numbDay == 3) {
-            System.out.println(Wed);
-        } else if (numbDay == 4) {
-            System.out.println(Thu);
-        } else if (numbDay == 5) {
-            System.out.println(Fri);
-        } else if (numbDay == 6) {
-            System.out.println(Sat);
-        } else if (numbDay == 7) {
-            System.out.println(Sun);
-        } else {
-            System.out.println("Нет такого дня недели!");
+        public static void main(String[] args) {
+                System.out.println(Mon);
+                System.out.println(Tue);
+                System.out.println(Wed);
+                System.out.println(Thu);
+                System.out.println(Fri);
+                System.out.println(Sat);
+                System.out.println(Sun);
+                Scanner timeTable = new Scanner(System.in);
+                System.out.println("Введите порядковый номер дня недели");
+                int numbDay = timeTable.nextInt();
+                if (numbDay == 1) {
+                        System.out.println(Mon);
+                } else if (numbDay == 2) {
+                        System.out.println(Tue);
+                } else if (numbDay == 3) {
+                        System.out.println(Wed);
+                } else if (numbDay == 4) {
+                        System.out.println(Thu);
+                } else if (numbDay == 5) {
+                        System.out.println(Fri);
+                } else if (numbDay == 6) {
+                        System.out.println(Sat);
+                } else if (numbDay == 7) {
+                        System.out.println(Sun);
+                } else {
+                        System.out.println("Нет такого дня недели!");
+                }
         }
-    }
 }
