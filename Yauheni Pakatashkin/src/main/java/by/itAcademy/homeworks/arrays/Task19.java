5c88f31becf564c0d3e10e91bb2a0836de53f48e
Yauheni  Pakatashkin <stinkor06@mail.ru>
2021-11-28 15:30:14 +0300

    add tasks 18,19 of arrays

--- Yauheni Pakatashkin/src/main/java/by/itAcademy/homeworks/arrays/Task19.java
+++ Yauheni Pakatashkin/src/main/java/by/itAcademy/homeworks/arrays/Task19.java
@@ -1,4 +1,16 @@
 package by.itAcademy.homeworks.arrays;
 
 public class Task19 {
+    public static void main(String[] args) {
+        int [] array = FillingTheArray.Array(10);
+        for (int i =0; i<array.length; i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.print(" // вывод массива");
+        System.out.println("");
+        for (int i =0; i<array.length; i+=2){
+            System.out.print(array[i] + " ");
+        }
+        System.out.print("   // вывод элементов на четных позициях");
+    }
 }
