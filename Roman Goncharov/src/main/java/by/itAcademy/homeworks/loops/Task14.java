83547468f2fc7eaa0e6334438e3a65dc7b775836
Roma <roma.goncharov.1997@mail.ru>
2021-11-28 15:31:52 +0300

    added Task14Test

--- Roman Goncharov/src/main/java/by/itAcademy/homeworks/loops/Task14.java
+++ Roman Goncharov/src/main/java/by/itAcademy/homeworks/loops/Task14.java
@@ -6,12 +6,16 @@ package by.itAcademy.homeworks.loops;
 public class Task14 {
     public static void main(String[] args) {
         long num = 7893823445L;
+        System.out.println("Сумма цифр заданного числа равна " + sumNumbers(num));
+    }
+
+    public static long sumNumbers(long num) {
         long sum = 0;
         do {
             long a = num % 10;
             num = num / 10;
             sum = sum + a;
         } while (num != 0);
-        System.out.println("Сумма цифр заданного числа равна " + sum);
+        return sum;
     }
 }
