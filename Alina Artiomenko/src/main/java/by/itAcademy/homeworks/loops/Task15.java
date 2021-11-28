8f33e045f34782a517a484ab4cfffaa1ba81dfb9
Alina Artiomenko <meyhe@example.com>
2021-11-28 21:53:19 +0300

    Tasks 4,9-11,14-17; Add Test 4,9,10

--- Alina Artiomenko/src/main/java/by/itAcademy/homeworks/loops/Task15.java
+++ Alina Artiomenko/src/main/java/by/itAcademy/homeworks/loops/Task15.java
@@ -1,4 +1,29 @@
 package by.itAcademy.homeworks.loops;
 
+/**Задание15
+ * Найти среди чисел от 50 до 70 второе простое число (число называют простым,
+ * если оно делится без остатка только на 1 и себя) и завершить цикл с
+ * использованием break.
+ */
+
 public class Task15 {
+    public static void main(String[] args){
+        int index = 0;
+
+        for (int i = 50; i <= 70; i++){
+            int count = 0;
+            for (int j = 2; j <= i; j++){
+                if (i % j == 0){
+                    count++;
+                }
+            }
+            if (count < 2) {
+                index++;
+                if (index == 2){
+                    System.out.println("Второе простое число = " + i);
+                    break;
+                }
+            }
+        }
+    }
 }
--- Alina Artiomenko/src/main/java/by/itAcademy/homeworks/operators/Task10.java
+++ Alina Artiomenko/src/main/java/by/itAcademy/homeworks/operators/Task10.java
@@ -1,4 +1,38 @@
 package by.itAcademy.homeworks.operators;
 
+import java.sql.SQLOutput;
+
+/**Задание10
+ * 1. даны два дома (a на b) и (c на d)
+ * 2. дан участок (e на f)
+ * 3. проверить, помещаются ли эти дома на участке
+ * 4. стороны домов || сторонам участка
+ */
+
 public class Task10 {
+    public static void main(String[] args) {
+        int house01SideA = 10, house01SideB = 60;
+        int house02SideA = 10, house02SideB = 15;
+        int areaSideA = 20, areaSideB = 50;
+
+        System.out.print(twoHouseArePlate(house01SideA,house01SideB,house02SideA,house02SideB,areaSideA,areaSideB));
+    }
+
+
+        public static String twoHouseArePlate(int house01A, int house01B, int house02A, int house02B,
+                                              int areaA, int areaB){
+        String result="";
+        if ((areaA >= house01A && areaA >= house02A && areaB >= (house01B + house02B)) ||
+           (areaA >= house01B && areaA >= house02A && areaB >= (house01A + house02B)) ||
+           (areaA >= house01A && areaA >= house02B && areaB >= (house01B + house02A)) ||
+           (areaA >= house01B && areaA >= house02B && areaB >= (house01A + house02A)) ||
+           (areaB >= house01A && areaB >= house02A && areaA >= (house01B + house02B)) ||
+           (areaB >= house01B && areaB >= house02A && areaA >= (house01A + house02B)) ||
+           (areaB >= house01A && areaB >= house02B && areaA >= (house01B + house02A)) ||
+           (areaB >= house01B && areaB >= house02B && areaA >= (house01A + house02A))){
+            return result = "Два дома помещаются на участке";
+        } else {
+            return result = "Два дома не помещаются на участке";
+        }
+        }
 }
