f84708c411d9cc5fc2cd55d73f411a1c34a215de
Dzmitry1405 <vdn_13@mail.ru>
2021-11-28 15:29:43 +0300

    Task17-20.java

--- Dzmitry Vasilchyk/src/main/java/by/itAcademy/homeworks/arrays/Task20.java
+++ Dzmitry Vasilchyk/src/main/java/by/itAcademy/homeworks/arrays/Task20.java
@@ -1,4 +1,25 @@
 package by.itAcademy.homeworks.arrays;
 
+/**Задание20
+ Создать переменную для массива из 10 элементов. Заполнить его произвольными значениями
+ целочисленного типа. Найти максимальный элемент и вывести его индекс.
+ **/
 public class Task20 {
+    public static void main(String [] args){
+        int size = 10;
+        int marks[] = new int[size];
+        int i;for(i=0;i<size;i++) {
+            marks[i] = (int) (Math.random() * 10 + 1);
+        }
+        i=0;
+        int num=-1;
+        int maxMark = marks[0];
+        for ( i=0; i < 10; i++) {
+            if (maxMark < marks[i]) {
+                maxMark = marks[i];
+                num=i+1;
+            }
+        }
+        System.out.println("индекс максимального элемента: " + num);
+        }
 }
