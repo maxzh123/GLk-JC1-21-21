26d7bc3d7ebdd29aa24da601b042502a6dd10869
Siarhei <fyu92291@zwoho.com>
2021-11-29 20:40:25 +0300

    пример

--- Examples/src/main/java/by/itAcademy/oop/OopRunner.java
+++ Examples/src/main/java/by/itAcademy/oop/OopRunner.java
@@ -10,13 +10,13 @@ public class OopRunner {
         Animal[] pets=new Animal[10];
         int i=0;
         pets[i++]=new Doberman();
-        pets[i++]=new Cat();
+        pets[i++]=getAnon();
         pets[i++]=new Doberman();
         pets[i++]=new Cat();
         pets[i++]=new Doberman();
         pets[i++]=new Cat();
         pets[i++]=new MainKun();
-        pets[i++]=new Cat();
+        pets[i++]=getAnimal();
         sayAll(pets);
 //        doSay(new MainKun());
 
@@ -30,6 +30,7 @@ public class OopRunner {
                 doSay(animals[i]);
             }
         }
+
     }
 
     private static void doSay(Dog dog){
@@ -45,6 +46,25 @@ public class OopRunner {
     private static void doSay(Animal animal,int year){
         animal.say();
         System.out.println(year);
+    }
+
+    private static class Дракон implements Animal{
+
+        @Override
+        public void say() {
+            System.out.println("ЛЛЛЛЛЛЛЛЛЛЛЛЛЛЛ. я калтавый длакон!");
+        }
+    }
+    public static Animal getAnimal(){
+        return new Дракон();
+    }
+    public static Animal getAnon(){
+        return new Animal() {
+            @Override
+            public void say() {
+                System.out.println("Битарды всегда в аноне.");
+            }
+        };
     }
 
 }
