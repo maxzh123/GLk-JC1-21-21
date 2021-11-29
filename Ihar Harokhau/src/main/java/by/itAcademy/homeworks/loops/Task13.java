93e48d6b33305206b128c43e4bcff7578708b59b
Ihar_Harokhau <78763230+IharHarokhau@users.noreply.github.com>
2021-11-29 17:00:25 +0300

    Update Task13.java

--- Ihar Harokhau/src/main/java/by/itAcademy/homeworks/loops/Task13.java
+++ Ihar Harokhau/src/main/java/by/itAcademy/homeworks/loops/Task13.java
@@ -1,4 +1,21 @@
 package by.itAcademy.homeworks.loops;
 
+/**
+ * Задание 13
+ *
+ * Вычислить произведение чисел от 1 до 25 с помощью цикла do while.
+ */
 public class Task13 {
+    public static void main(String[] args) {
+        int value = 25;
+        int i = 1;
+
+        do
+        {
+           i *= value;
+           value--;
+        }
+        while (value >= 1);
+        System.out.println("Произведение чисел от 1 до 25 = " + i);
+    }
 }
