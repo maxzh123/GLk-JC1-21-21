272946623448970589f363de786d247ead870045
Yauheni  Pakatashkin <stinkor06@mail.ru>
2021-12-01 20:49:14 +0300

    add tasks 10

--- Yauheni Pakatashkin/src/main/java/by/itAcademy/homeworks/arrays/FillingTheArray.java
+++ Yauheni Pakatashkin/src/main/java/by/itAcademy/homeworks/arrays/FillingTheArray.java
@@ -3,12 +3,12 @@ package by.itAcademy.homeworks.arrays;
 import java.util.Random;
 
 public class FillingTheArray {
-    public static int[] Array(int a) {
+    public static int[] array(int a) {
         int[] array = new int[a];
         Random random = new Random();
 
         for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(101);
+            array[i] = random.nextInt(11)+1;
         }
 
         return array;
