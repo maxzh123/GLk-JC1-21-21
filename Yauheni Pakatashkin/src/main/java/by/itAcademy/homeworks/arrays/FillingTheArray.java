b9f6954262596be163781fce7156c0348d191f56
Siarhei <fyu92291@zwoho.com>
2021-12-02 18:33:52 +0300

    пример

--- Yauheni Pakatashkin/src/main/java/by/itAcademy/homeworks/arrays/FillingTheArray.java
+++ Yauheni Pakatashkin/src/main/java/by/itAcademy/homeworks/arrays/FillingTheArray.java
@@ -4,11 +4,15 @@ import java.util.Random;
 
 public class FillingTheArray {
     public static int[] array(int a) {
+        return array(a,10);
+    }
+
+    public static int[] array(int a,int limit) {
         int[] array = new int[a];
         Random random = new Random();
 
         for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(11)+1;
+            array[i] = random.nextInt(limit)+1;
         }
 
         return array;
