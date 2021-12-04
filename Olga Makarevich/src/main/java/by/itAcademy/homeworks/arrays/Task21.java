be874b0032fd862d5f9f8793a58c9ed118c00cbc
Olga Makarevich <olgagomel17@mail.ru>
2021-12-04 19:15:17 +0300

    Task 21 not done

--- Olga Makarevich/src/main/java/by/itAcademy/homeworks/arrays/Task21.java
+++ Olga Makarevich/src/main/java/by/itAcademy/homeworks/arrays/Task21.java
@@ -1,15 +1,28 @@
 package by.itAcademy.homeworks.arrays;
 
+import java.util.Arrays;
 //Создайте переменную для массива из 10 элементов. Заполните его произвольными значениями целочисленного типа. Определите сумму элементов массива, расположенных между минимальным и максимальным значениями.
 // Если значений максимальных и минимальных несколько, то необходимо взять максимальное значение разницы индексов между максимальным и минимальным значениями.
 
 public class Task21 {
+    public static void main(String[] args) {
 
-    private static int [] arrayRandom (int [] a) {
+    }
+
+    private static int[] arrayRandom(int[] a) {
         a = new int[10];
         for (int i = 0; i < 10; i++) {
-            a [i] = (int) (Math.random()*1000+1);
+            a[i] = (int) (Math.random() * 1000 + 1);
+            System.out.println(a);
         }
         return a;
     }
+
+//    private static void arraySort(int[] arrayRandom) {
+//        Arrays.sort(arrayRandom);
+//        for (int i = 0; i < arrayRandom.length; i++) {
+//            System.out.print(arrayRandom[i] + "  ");
+//        }
+//    }
 }
+
