bb59deb6c2b6b96aee38c76540ed2c5792c2150f
Olga Makarevich <olgagomel17@mail.ru>
2021-12-04 23:12:15 +0300

    Task 22 not done

--- Olga Makarevich/src/main/java/by/itAcademy/homeworks/arrays/Task22.java
+++ Olga Makarevich/src/main/java/by/itAcademy/homeworks/arrays/Task22.java
@@ -1,4 +1,26 @@
 package by.itAcademy.homeworks.arrays;
 
+// Создайте переменную для массива из 10 элементов.
+// Заполните его произвольными значениями целочисленного типа.
+// Выведите на экран, переверните и снова выведите на экран (при переворачивании нежелательно создавать еще один массив).
+
+import java.util.Arrays;
+
+import static java.lang.System.*;
+
 public class Task22 {
+    public static void main(String[] args) {
+        int [] arrayСirculation = ArraysHelper.arrayRandom(10);
+        out.println(Arrays.toString(arrayСirculation));
+        out.println(Arrays.toString(arrConversely(arrayСirculation)));
+    }
+
+    public static int[] arrConversely (int [] arrConversely) { // НЕ РАБОТАЕТ
+        int conversely = 0;
+        for (int i = arrConversely[arrConversely.length-1]; i < arrConversely.length; i--) { // логика начиная с последнего, каждый индекс объявлять первым, вторым и т.д.
+            arrConversely[i] = arrConversely[conversely];
+            conversely++;
+        }
+        return arrConversely;
+    }
 }
