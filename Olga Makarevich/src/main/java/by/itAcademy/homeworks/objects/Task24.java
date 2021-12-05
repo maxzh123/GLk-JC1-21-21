9f322d85d7258b50ea8984efa92686cff0fcc24b
Olga Makarevich <olgagomel17@mail.ru>
2021-12-05 21:06:19 +0300

    Task 24 done 50/50

--- Olga Makarevich/src/main/java/by/itAcademy/homeworks/objects/Task24.java
+++ Olga Makarevich/src/main/java/by/itAcademy/homeworks/objects/Task24.java
@@ -1,4 +1,21 @@
 package by.itAcademy.homeworks.objects;
 
-public class Task24 {
+// Создать класс описывающие промежуток времени. Сам промежуток в классе должен задаваться тремя свойствами: секундами, минутами, часами.
+// Сделать методы для получения полного количества секунд в объекте, сравнения двух объектов (метод должен работать аналогично compareTo в строках). Создать два конструктора:
+//
+//получающий общее количество секунд
+//получающий часы, минуты и секунды по отдельности. Сделать метод для вывода данных. Прочее на ваше усмотрение.
+
+
+public class Task24 { // Не понимаю, как работает метод compareTo; метод comparisonTimes не работает
+    public static void main(String[] args) {
+        TimeInterval TimePeriod = new TimeInterval();
+        TimePeriod.hour = (int) (Math.random() * 24 + 1);
+        TimePeriod.minute = (int) (Math.random() * 60 + 1);
+        TimePeriod.second =(int) (Math.random() * 60 + 1);
+        System.out.println(TimeInterval.numberOfSeconds(TimePeriod.hour, TimePeriod.minute, TimePeriod.second));
+        System.out.println(TimePeriod.hour + " hour " + TimePeriod.minute + " minute " + TimePeriod.second + " second");
+        System.out.println(TimeInterval.comparisonTimes(TimePeriod.hour, TimePeriod.minute, TimePeriod.second));
+    }
+
 }
