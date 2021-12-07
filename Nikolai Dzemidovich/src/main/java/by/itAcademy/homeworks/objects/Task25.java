7b0299a457c1c18907a7200b1846b0bb812fab05
NickolaiDemidovich <Nicknewb97@gmail.com>
2021-12-07 23:31:56 +0300

    Fixed task 24 + Complete Task 25

--- Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/objects/Task25.java
+++ Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/objects/Task25.java
@@ -1,4 +1,22 @@
 package by.itAcademy.homeworks.objects;
 
 public class Task25 {
+    //Метод с буленом у меня получился рудементарным.
+    public static void main(String[] args) {
+        CashMachine atm = new CashMachine(50,100,100);
+
+        System.out.println("Баланс банкомата " + atm.getAtmBalance());
+        System.out.println("Баланс пользователя " + atm.getUserBalance());
+
+        System.out.println(atm.toWithdrawMoney());
+        System.out.println(atm.toCheckWithdraw());
+
+        System.out.println(atm.toDepositMoney());
+        System.out.println(atm.toCheckDeposit());
+
+        System.out.println("Баланс банкомата " + atm.getAtmBalance());
+        System.out.println("Баланс пользователя " + atm.getUserBalance());
+
+
+    }
 }
