1557adb741b4b6550707f1c2974b7b375b6989ea
Roma <roma.goncharov.1997@mail.ru>
2021-12-17 19:46:15 +0300

    Completed Task38

---
+++ Roman Goncharov/src/main/java/by/itAcademy/homeworks/generics/Motorcycle.java
@@ -0,0 +1,10 @@
+package by.itAcademy.homeworks.generics;
+
+public class Motorcycle extends Vehicle{
+
+    public Motorcycle(String brand, String model){
+        super("Мотоцикл");
+        this.brand = brand;
+        this.model = model;
+    }
+}
