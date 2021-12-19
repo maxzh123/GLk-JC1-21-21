2506ccc4a6bdad5e68076dd5b7d9534127da3b97
NickolaiDemidovich <Nicknewb97@gmail.com>
2021-12-20 00:05:12 +0300

    Complete Enums

--- Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/enums/Task36.java
+++ Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/enums/Task36.java
@@ -1,4 +1,9 @@
 package by.itAcademy.homeworks.enums;
+/**> Написать метод, который выводит следующий сезон от заданного во входном
+        > параметре. Входной параметр ввести с клавиатуры.*/
 
 public class Task36 {
+    public static void main(String[] args) {
+        Seasons.getNextSeason();
+    }
 }
--- Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/enums/Task37.java
+++ Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/enums/Task37.java
@@ -1,4 +1,7 @@
 package by.itAcademy.homeworks.enums;
 
 public class Task37 {
+    public static void main(String[] args) {
+        Seasons.getTheNumberOfDays();
+    }
 }
