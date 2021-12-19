519d512a80aacdb73c029072b85a91e17ede9eff
NickolaiDemidovich <Nicknewb97@gmail.com>
2021-12-19 21:11:16 +0300

    Complete 38

--- Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/generics/Task38.java
+++ Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/generics/Task38.java
@@ -1,4 +1,20 @@
 package by.itAcademy.homeworks.generics;
 
 public class Task38 {
+    /** Создать поле name в классе Vehicle и проинициализировать его через
+> конструктора. Создать generic класс Garage, который может хранить только объекты
+> типа наследуемого от Vehicle. Создать 2 объекта класса Garage (все поля ввести с
+> клавиатуры) и вывести на экран имя хранимого транспортного средства.*/
+
+    public static void main(String[] args) {
+        Garage<Vehicle> veh1 = new Garage<>();
+        Garage<Vehicle> veh2 = new Garage<>();
+
+        veh1.addVehicle(new Car());
+        veh2.addVehicle(new Motorcycle());
+
+        System.out.println("В первом гараже - " + veh1.getInfo());
+        System.out.println("Во втором гараже - " + veh2.getInfo());
+
+    }
 }
