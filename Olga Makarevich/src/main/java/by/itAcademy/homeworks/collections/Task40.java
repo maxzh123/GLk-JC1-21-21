55f056cdcb75b4e614bc3816da3b69073b577ab8
Olga Makarevich <olgagomel17@mail.ru>
2021-12-21 10:43:05 +0300

    Task 40 done

--- Olga Makarevich/src/main/java/by/itAcademy/homeworks/collections/Task40.java
+++ Olga Makarevich/src/main/java/by/itAcademy/homeworks/collections/Task40.java
@@ -1,4 +1,22 @@
 package by.itAcademy.homeworks.collections;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
+// Создать коллекцию, заполнить ее случайными целыми числами.
+// Удалить повторяющиеся числа.
+
+
 public class Task40 {
+    public static void main(String[] args) {
+        ArrayList<Integer> newArrayList = new ArrayList<>();
+        System.out.println(Task39.createArrayListRandom(newArrayList, 10));
+        System.out.println(excludeRepetition(newArrayList));
+    }
+
+    static ArrayList<Integer> excludeRepetition(ArrayList<Integer> aL) {
+        HashSet newHashSet = new HashSet(aL);
+        ArrayList aLl = new ArrayList(newHashSet);
+        return aLl;
+    }
 }
