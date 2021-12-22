4188611ee9804323c7a20f575ef473a5274c70a5
Alina Artiomenko <meyhe@example.com>
2021-12-23 00:24:56 +0300

    Task 43, 44

---
+++ Alina Artiomenko/src/main/java/by/itAcademy/homeworks/exceptions/ScannerException.java
@@ -0,0 +1,7 @@
+package by.itAcademy.homeworks.exceptions;
+
+public class ScannerException extends Exception{
+    public ScannerException(String message){
+        super(message);
+    }
+}
--- Alina Artiomenko/src/main/java/by/itAcademy/homeworks/exceptions/Task44.java
+++ Alina Artiomenko/src/main/java/by/itAcademy/homeworks/exceptions/Task44.java
@@ -1,4 +1,47 @@
 package by.itAcademy.homeworks.exceptions;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**Задание 43
+ * 1. Написать собственное исключение от Exception
+ * 2. Написать код, который будет выбрасывать заданное исключение и обрабатывать его
+ * 3. Результат работы программы вывести на экран
+ */
 
 public class Task44 {
+    public static void main(String[] args){
+        ArrayList<String> listOfMonth = createListOfMonth();
+        System.out.println(listOfMonth);
+
+        Scanner inputMonth = new Scanner(System.in);
+
+        while(listOfMonth.size() != 12) {
+            try {
+                System.out.print("Введите месяц: ");
+                String month = inputMonth.nextLine();
+                checkTheValue(month,listOfMonth);
+                listOfMonth.add(month);
+                System.out.println(listOfMonth);
+            } catch (ScannerException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static ArrayList<String> createListOfMonth(){
+        ArrayList<String> month = new ArrayList<>();
+        month.add("September");
+        month.add("November");
+        month.add("May");
+        month.add("April");
+
+        return month;
+    }
+
+    public static void checkTheValue(String value, ArrayList<String> list) throws ScannerException{
+        if(list.contains(value)){
+            throw new ScannerException("Такой месяц уже задан!");
+        }
+    }
+
 }
