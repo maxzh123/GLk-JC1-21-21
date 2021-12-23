077e11ff72669c2429c6aa4fd944c87aa88b6080
Olga Makarevich <olgagomel17@mail.ru>
2021-12-23 10:50:03 +0300

    Task 36 done

--- Olga Makarevich/src/main/java/by/itAcademy/homeworks/enums/Task36.java
+++ Olga Makarevich/src/main/java/by/itAcademy/homeworks/enums/Task36.java
@@ -1,4 +1,15 @@
 package by.itAcademy.homeworks.enums;
 
+// Написать метод, который выводит следующий сезон от заданного во входном параметре.
+// Входной параметр ввести с клавиатуры.
+
+import java.util.Scanner;
+
 public class Task36 {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Введите сезон года");
+        String season = scr.nextLine();
+        System.out.println(Season.getNextSeason(season));
+    }
 }
