2897b43a9b01579e75b2bc0d634c8dabb5af3ae1
ItAcademy <none@example.com>
2021-12-23 21:39:48 +0300

    примеры

--- Examples/src/main/java/by/itAcademy/homeworks/threads/Task52.java
+++ Examples/src/main/java/by/itAcademy/homeworks/threads/Task52.java
@@ -1,4 +1,16 @@
 package by.itAcademy.homeworks.threads;
 
+import java.util.concurrent.TimeUnit;
+
 public class Task52 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t= new Thread(new ThreadWork());
+        t.setDaemon(true);
+        System.out.println("Main 1");
+        t.start();
+        TimeUnit.MILLISECONDS.sleep(50);
+        System.out.println("Main 2");
+        //t.join();
+        System.out.println("Main 3");
+    }
 }
