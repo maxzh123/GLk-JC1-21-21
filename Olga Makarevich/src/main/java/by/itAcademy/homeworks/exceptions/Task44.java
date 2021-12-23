e3aba85dc586a804bba701f8bcb76e5799e21352
Olga Makarevich <olgagomel17@mail.ru>
2021-12-23 14:49:22 +0300

    Task 43 done

--- Olga Makarevich/src/main/java/by/itAcademy/homeworks/exceptions/Task44.java
+++ Olga Makarevich/src/main/java/by/itAcademy/homeworks/exceptions/Task44.java
@@ -1,4 +1,28 @@
 package by.itAcademy.homeworks.exceptions;
 
+// Написать собственное исключение от Exception.
+// Сгенерировать код, который будет выбрасывать его и обрабатывать.
+// Результат работы программы вывести на экран.
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Task44 {
+    public static void main(String[] args) {
+        int[] a = new int[10];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (int) (Math.random() * 10 + 1);
+        }
+        System.out.println(Arrays.toString(a));
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == 9) {
+                try {
+                    throw new MyException("Цифра 9 не любимая");
+                } catch (MyException e) {
+                    System.out.println("Массив должен исключать цифру 9");
+                }
+            }
+        }
+    }
 }
