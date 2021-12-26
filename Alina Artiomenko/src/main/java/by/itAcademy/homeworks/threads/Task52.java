95a934539e93adefde1cf2b3315fa1e7f1c85536
Alina Artiomenko <meyhe@example.com>
2021-12-27 00:57:06 +0300

    Task 52 (уверенность,что сделано правильно = 3%)

--- Alina Artiomenko/src/main/java/by/itAcademy/homeworks/threads/Task52.java
+++ Alina Artiomenko/src/main/java/by/itAcademy/homeworks/threads/Task52.java
@@ -1,4 +1,29 @@
 package by.itAcademy.homeworks.threads;
 
+import java.util.ArrayList;
+
+/**Задание 52
+ * 1. Создать коллекцию из 10 случайных целых чисел
+ * 2. Создать 10 потоков, каждый из которых считает среднее арифметическое коллекции
+ * 3. Вывести на экран
+ */
+
 public class Task52 {
+    public static void main(String[] args) {
+        final int value = 10;
+
+        for (int i = 0; i < value; i++){
+            ArrayList<Integer> array = createAndFillList(value);
+            MyThreads threads = new MyThreads(array);
+            threads.start();
+        }
+    }
+
+    public static ArrayList<Integer> createAndFillList(int number){
+        ArrayList<Integer> list = new ArrayList<Integer>(number);
+        for (int i = 0; i < number; i++){
+            list.add((int)(Math.random() * 10 + 13));
+        }
+        return list;
+    }
 }
