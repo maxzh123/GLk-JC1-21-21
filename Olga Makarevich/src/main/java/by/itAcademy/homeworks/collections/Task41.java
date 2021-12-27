f15d0b159df0cd276055c43a63018211e3462526
Olga Makarevich <olgagomel17@mail.ru>
2021-12-27 12:38:51 +0300

    Task 41 ?

--- Olga Makarevich/src/main/java/by/itAcademy/homeworks/collections/Task41.java
+++ Olga Makarevich/src/main/java/by/itAcademy/homeworks/collections/Task41.java
@@ -3,17 +3,27 @@ package by.itAcademy.homeworks.collections;
 //Создать список оценок учеников с помощью ArrayList, заполнить случайными оценками.
 //Найти самую высокую оценку с использованием итератора.
 
-//import java.util.ArrayList;
-//import java.util.Iterator;
-//
-//public class Task41 {
-//    public static void main(String[] args) {
-//        int a = 10; // количество учеников
-//        ArrayList<Integer> listOfStudentsAssessment = new ArrayList<Integer>();
-//        System.out.println(Task39.createArrayListRandom(listOfStudentsAssessment, a));
-//        System.out.println(foundMax(listOfStudentsAssessment));
-//    }
+import java.util.ArrayList;
+import java.util.Iterator;
 
-//    static int foundMax(ArrayList<Integer> random) {
-//    }
-//}
+public class Task41 {
+    public static void main(String[] args) {
+        int a = 10; // количество учеников
+        ArrayList<Integer> listOfStudentsAssessment = new ArrayList<Integer>();
+        System.out.println(Task39.createArrayListRandom(listOfStudentsAssessment, a));
+        System.out.println("Самая высокая оценка = " + foundMax(listOfStudentsAssessment));
+    }
+
+    static int foundMax(ArrayList<Integer> random) {
+        int max = 0;
+        Iterator i = random.iterator();
+        while (i.hasNext()) {
+            for (int j = 0; j < random.size(); j++) {
+                if (random.get(j) > max) {
+                    max = random.get(j);
+                }
+            }
+        }
+        return max;
+    }
+}
