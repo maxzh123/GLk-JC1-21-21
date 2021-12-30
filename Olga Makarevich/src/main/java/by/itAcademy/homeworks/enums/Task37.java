25a74de7da12bc4f2f803f03328840a483cbb638
Olga Makarevich <olgagomel17@mail.ru>
2021-12-30 13:13:49 +0300

    Task 37 ? - не работает при неправильном вводе с клавиатуры

--- Olga Makarevich/src/main/java/by/itAcademy/homeworks/enums/Task37.java
+++ Olga Makarevich/src/main/java/by/itAcademy/homeworks/enums/Task37.java
@@ -3,5 +3,15 @@ package by.itAcademy.homeworks.enums;
 // Написать метод, который в зависимости от сезона, выводит на экран сумму дней в этом сезоне.
 // Входной параметр ввести с клавиатуры.
 
+import java.util.Scanner;
+
 public class Task37 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите сезон");
+        String str1 = scanner.nextLine();
+        System.out.println(Season.getNumberOfDay(Season.valueOfString(str1)));
+    }
 }
+
+// при неправильном вводе наименования сезона не работает - объясните, пожалуйста почему
