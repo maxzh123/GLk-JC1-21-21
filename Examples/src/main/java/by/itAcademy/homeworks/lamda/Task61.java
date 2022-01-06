befce83f6edfd2cf08d0618114b4255675e89add
Николай Галов <nikgal007@gmail.com>
2022-01-06 21:15:58 +0300

    Пример

--- Examples/src/main/java/by/itAcademy/homeworks/lamda/Task61.java
+++ Examples/src/main/java/by/itAcademy/homeworks/lamda/Task61.java
@@ -1,4 +1,28 @@
 package by.itAcademy.homeworks.lamda;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Task61 {
+    public static void main(String[] args) {
+        printNumber((x,y)->x+';'+y);
+        printNumber((x, y)->{
+            return x+';'+y;
+        });
+        printNumber((String x, String y)->{
+                return x+';'+y;
+        });
+        printNumber(new MyIface(){
+            @Override
+            public String doSomeStuff(String x, String y) {
+                return x+';'+y;
+            }
+        });
+    }
+
+
+    static void printNumber(MyIface holder){
+        System.out.println(holder.doSomeStuff("azaza","bbbb"));
+    }
 }
