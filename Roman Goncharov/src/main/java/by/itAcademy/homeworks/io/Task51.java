9fbe3b634a3ca16e10f7089fcdffd9a4e4d5a7a6
Roma <roma.goncharov.1997@mail.ru>
2022-01-08 14:12:11 +0300

    Task51 Updated (Generics)

--- Roman Goncharov/src/main/java/by/itAcademy/homeworks/io/Task51.java
+++ Roman Goncharov/src/main/java/by/itAcademy/homeworks/io/Task51.java
@@ -9,36 +9,15 @@ package by.itAcademy.homeworks.io;
 import by.itAcademy.homeworks.io.Task51Objects.Person;
 import by.itAcademy.homeworks.io.Task51Objects.RandomVariables;
 
-import java.io.*;
-
 public class Task51 {
     public static void main(String[] args) {
         Person[] people = new Person[10];
         for (int i = 0; i < people.length; i++) {
             people[i] = new Person(RandomVariables.randomName(), RandomVariables.randomSurname(), RandomVariables.randomAge());
         }
-        FileHandler fileHandlerText = new FileHandler("TextFile51.txt");
-        fileHandlerText.fileWrite(textDescriptionOfObjects(people));
-        FileHandler fileHandlerBinary = new FileHandler("BinaryFile51.bin");
-        writeObjectToFile(people, fileHandlerBinary.getFilePath());
-    }
-
-    public static String textDescriptionOfObjects(Object[] objects) {
-        StringBuilder listString = new StringBuilder();
-        for (Object obj : objects) {
-            listString.append(obj).append("\n");
-        }
-        return listString.toString();
-    }
-
-    public static void writeObjectToFile(Object[] objects, String fileName) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName))) {
-            oos.writeObject(objects);
-            System.out.println("Запись в файл " + fileName + " произведена успешно.");
-        } catch (FileNotFoundException e) {
-            System.out.println("Файл не найден.");
-        } catch (IOException e) {
-            System.out.println("Ошибка. Что-то не так с файлом.");
-        }
+        FileHandlerForObjectPerson fhOne = new FileHandlerForObjectPerson("TextFile51.txt",people);
+        fhOne.writeTextToFile(fhOne.textDescriptionOfPeople());
+        FileHandlerForObjectPerson fhTwo = new FileHandlerForObjectPerson("BinaryFile51.bin",people);
+        fhTwo.writeObjectToFile();
     }
 }
