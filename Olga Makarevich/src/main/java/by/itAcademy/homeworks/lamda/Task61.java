67fb3111eee02c297bb479be012534740060a13c
Olga Makarevich <olgagomel17@mail.ru>
2022-01-08 14:10:30 +0300

    Task 61 done

--- Olga Makarevich/src/main/java/by/itAcademy/homeworks/lamda/Task61.java
+++ Olga Makarevich/src/main/java/by/itAcademy/homeworks/lamda/Task61.java
@@ -1,4 +1,53 @@
 package by.itAcademy.homeworks.lamda;
 
+// Напишите программу, которая для всех четных значений длиной от 1 до 20 дюймов:
+//
+//печатает на экран значения в дюймах;
+//переводит значения в сантиметры;
+//печатает на экран сумму в сантиметрах.
+
+import java.util.ArrayList;
+
 public class Task61 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 20;
+        ArrayList<Integer> arraylist = new ArrayList(b);
+        for (int i = 0; i < b; i++) {
+            arraylist.add(i, a);
+            a++;
+        }
+        //System.out.println(arraylist.toString());
+        System.out.println(newObj.test(arraylist));
+    }
+
+    public static UsedForTask61 newObj = arr -> {
+        int sum = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) % 2 == 0) {
+                System.out.println(arr.get(i) + " дюймов, " + arr.get(i) * 2.54 + " сантиметров");
+                sum = sum + arr.get(i);
+            }
+        }
+        System.out.println("Сумма = " + sum * 2.54 + " сантиметров");
+        return true;
+    };
 }
+
+/** В интерфейсе UsedForTask61 и объекте newObj (Task61) Idea захотела, чтобы метод test был boolean, а я делала void. С void не работает, а почему - ?*/
+
+/** ПОЧЕМУ НЕ РАБОТАЕТ ВОТ ТАК?*/
+//    public static void main(String[] args) {
+//        ArrayList<Integer> arrlist = new ArrayList<>();
+//        arrlist.fillingArrayList(1, 20);                  ВОТ ЗДЕСЬ МНЕ НЕ ПОНЯТНО, есть ссылка на объект, есть метод, а как его применить - ? - что я неправильно делаю?
+//    }
+//
+//    static ArrayList<Integer> fillingArrayList(int a, int b) {
+//        ArrayList<Integer> arraylist = new ArrayList(b);
+//        for (int i = 0; i < b; i++) {
+//            arraylist.add(i, a);
+//            a++;
+//        }
+//        System.out.println(arraylist.toString());
+//        return arraylist;
+//    }
--- Olga Makarevich/src/main/java/by/itAcademy/homeworks/lamda/Task63.java
+++ Olga Makarevich/src/main/java/by/itAcademy/homeworks/lamda/Task63.java
@@ -1,4 +1,7 @@
 package by.itAcademy.homeworks.lamda;
 
+//Сгенерируйте List коллекцию целых чисел из n элементов от minValue до maxValue.
+//Определить, содержатся ли в данной коллекции числа, которые делятся и на 3, и на 5 с помощью stream.
+
 public class Task63 {
 }
