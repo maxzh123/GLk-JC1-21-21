4a4f0137042955d7bdade8a4022ae99b9cdbb87e
Olga Makarevich <olgagomel17@mail.ru>
2022-01-09 19:17:39 +0300

    Task 33 done

--- Olga Makarevich/src/main/java/by/itAcademy/homeworks/oop/Task33.java
+++ Olga Makarevich/src/main/java/by/itAcademy/homeworks/oop/Task33.java
@@ -1,4 +1,21 @@
 package by.itAcademy.homeworks.oop;
 
+import by.itAcademy.homeworks.oop.Factory.*;
+
 public class Task33 {
+    public static void main(String[] args) {
+
+        Employee director1 = new Director("Иванов", "Иван", 160, 152, 5000, 2500); // 7125
+        System.out.printf("Зарплата %-10s %-10s составляет %.2f руб.\n", director1.getNameOfEmployee(), director1.getLastNameOfEmployee(), director1.calculateSalary());
+
+        Employee specialistOther = new Specialists("Милая", "Милана", 160, 152, 1000, 500, 0.9);
+        System.out.printf("Зарплата %-10s %-10s составляет %.2f руб.\n", specialistOther.getNameOfEmployee(), specialistOther.getLastNameOfEmployee(), specialistOther.calculateSalary());
+        Employee specialistSale = new SalesManager("Светлая", "Светлана", 160, 152, 1000, 500, 0.9);
+        System.out.printf("Зарплата %-10s %-10s составляет %.2f руб.\n", specialistSale.getNameOfEmployee(), specialistSale.getLastNameOfEmployee(), specialistSale.calculateSalary());
+
+        Employee locksmith1 = new Locksmith("Николаев", "Николай", 160, 152, 5000, 100, 10, 100, 90); // 900
+        System.out.printf("Зарплата %-10s %-10s составляет %.2f руб.\n", locksmith1.getNameOfEmployee(), locksmith1.getLastNameOfEmployee(), locksmith1.calculateSalary());
+        Employee locksmith2 = new Locksmith("Петров", "Петр", 160, 152, 0, 100, 10, 100, 100); // 1100
+        System.out.printf("Зарплата %-10s %-10s составляет %.2f руб.\n", locksmith2.getNameOfEmployee(), locksmith2.getLastNameOfEmployee(), locksmith2.calculateSalary());
+    }
 }
