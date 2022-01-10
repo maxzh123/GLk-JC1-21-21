fb5a944d9837fc659abe8cebaf7aa3d9e2b4e998
EkaterinaCherkas <94483993+EkaterinaCherkas@users.noreply.github.com>
2022-01-10 17:29:34 +0300

    Update Task29.java

--- Ekaterina Cherkas/src/main/java/by/itAcademy/homeworks/regexp/Task29.java
+++ Ekaterina Cherkas/src/main/java/by/itAcademy/homeworks/regexp/Task29.java
@@ -1,4 +1,27 @@
 package by.itAcademy.homeworks.regexp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/** Задание 29
+ * Введите с клавиатуры строку. Напишите метод, выполняющий поиск в строке шестнадцатеричных чисел, записанных
+ * по правилам Java, с помощью регулярных выражений. Результат работы метода выведите на экран.
+ */
+
 public class Task29 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine();
+        string(line);
+    }
+    private static void string(String line) {
+        Pattern pattern = Pattern.compile("0x([A-Fa-f0-9]{1,4})");
+        Matcher matcher = pattern.matcher(line);
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
 }
