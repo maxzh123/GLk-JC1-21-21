9cb0388f030347c42966ac56c3d41b5f212551c3
Alina Artiomenko <meyhe@example.com>
2022-01-12 17:10:17 +0300

    Task 70-71 done

--- Alina Artiomenko/src/main/java/by/itAcademy/homeworks/reflection/Task70.java
+++ Alina Artiomenko/src/main/java/by/itAcademy/homeworks/reflection/Task70.java
@@ -11,41 +11,58 @@ import java.util.Arrays;
  * 2. Создать метод, который распечатет информацию о классе с помощью рефлексии
  * 3. Вызвать метод с помощью рефлексии из основной программы
  */
-//ПОКА ЕЩЁ НЕ РЕАЛИЗОВАЛА ВЫЗОВ МЕТОДА РЕФЛЕКСИЕЙ
+
 public class Task70 {
     public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Main firstMain = new Main("Дед", "Мазай", 86, 25);
-        //firstMain.putInTheBoat();
-        //firstMain.checkNumberOfPessengersInTheBoat();
-        System.out.println("Интерфейсы класса Main:\n-------------------");
-        printInterfacesOfClass();
+        firstMain.putInTheBoat();
+        firstMain.checkNumberOfPessengersInTheBoat();
+
+        System.out.println("\nИнтерфейсы класса Main:\n-------------------");
+        printInterfacesOfClass(firstMain);
         System.out.println("\nМетоды класса Main:\n-------------------");
-        printDeclaredMethods();
+        printDeclaredMethods(firstMain);
         System.out.println("\nПоля класса Main:\n-------------------");
-        printDeclaredFields();
+        printDeclaredFields(firstMain);
+        System.out.println("\nВызов метода checkNumberOfPessengersInTheBoat:\n-------------------");
+        callinCheckNumberOfPessengersInTheBoat(firstMain);
     }
 
-    public static void printInterfacesOfClass() {
-        Class mClassObject = Main.class;
-        Class[] interfaces = mClassObject.getInterfaces();
-        for (Class in : interfaces) {
-            System.out.println(in);
+    public static void printInterfacesOfClass(Object obj) {
+        Class [] mClassObject = obj.getClass().getInterfaces();
+                for (Class in : mClassObject) {
+            System.out.println(in.getName());
         }
     }
 
-    public static void printDeclaredMethods(){
-        Class mClassObject = Main.class;
-        Method[] methods = mClassObject.getDeclaredMethods();
-        for (Method meth : methods) {
-            System.out.println(meth);
+    public static Method[] printDeclaredMethods(Object obj){
+        Method[] mClassObject = obj.getClass().getDeclaredMethods();
+        for (Method meth : mClassObject) {
+            System.out.println(meth.getName());
+        }
+        return mClassObject;
+    }
+
+    public static void printDeclaredFields(Object obj){
+        Field[] mClassObject = obj.getClass().getDeclaredFields();
+        for (Field fil : mClassObject) {
+            System.out.println(fil.getName());
         }
     }
 
-    public static void printDeclaredFields(){
-        Class mClassObject = Main.class;
-        Field[] fields = mClassObject.getDeclaredFields();
-        for (Field fil : fields) {
-            System.out.println(fil);
+    public static void callinCheckNumberOfPessengersInTheBoat(Object obj){
+        Method[] methods = obj.getClass().getDeclaredMethods();
+        for (Method m : methods){
+            if (m.getName().equals("checkNumberOfPessengersInTheBoat")){
+                m.setAccessible(true);
+                try {
+                    m.invoke(obj);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
