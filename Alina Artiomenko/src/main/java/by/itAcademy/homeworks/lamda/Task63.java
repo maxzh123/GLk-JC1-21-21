6026bfb24d612c437fc7c96cdd8ef21e864157be
Alina Artiomenko <meyhe@example.com>
2022-01-13 08:50:27 +0300

    Task 63 - fix

--- Alina Artiomenko/src/main/java/by/itAcademy/homeworks/lamda/Task63.java
+++ Alina Artiomenko/src/main/java/by/itAcademy/homeworks/lamda/Task63.java
@@ -2,8 +2,10 @@ package by.itAcademy.homeworks.lamda;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
@@ -22,8 +24,8 @@ public class Task63 {
         List<Integer> list = fillListOFRandomNumbers(amountOfNumber,minValue,maxValue);
 
         System.out.println("Массив чисел: \n" + Arrays.asList(list));
-        Stream<Integer> n2 = list.stream().filter((n)-> (n % 3) == 0 && (n % 5) == 0);
-        System.out.println("\nЧисла кратные и 3, и 5:\n" + n2.toList());
+        Stream<Integer> streamFromList = list.stream().filter((n)-> (n % 3) == 0 && (n % 5) == 0);
+        System.out.println("\nЧисла кратные и 3, и 5: " + streamFromList.collect(Collectors.toList()));
     }
 
     static List fillListOFRandomNumbers (int amountOfNumber, int min, int max){
