2f123ffe9a7c8e9423a710a075e1fd3ef3f71542
Dzmitry1405 <vdn_13@mail.ru>
2022-01-13 12:00:17 +0300

    update26-27+Test

--- Dzmitry Vasilchyk/src/main/java/by/itAcademy/homeworks/regexp/Task27.java
+++ Dzmitry Vasilchyk/src/main/java/by/itAcademy/homeworks/regexp/Task27.java
@@ -1,8 +1,5 @@
 package by.itAcademy.homeworks.regexp;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,19 +9,19 @@ import java.util.regex.Pattern;
  **/
 
 public class Task27 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введите любую стоку:");
-        String s = reader.readLine();
-        string(s);
-    }
-    private static void string(String s){
-        Pattern p = Pattern.compile("\\S+\\s"); //находит связку пробельный символ+не пробельный символ
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println("Введите любую стоку:");
+//        String s = reader.readLine();
+//        string(s);
+//    }
+    public static int string(String s){
+        Pattern p = Pattern.compile("[А-Яа-яЁёA-Za-z]+");
         Matcher m = p.matcher(s);
         int count = 0;
         while (m.find()){
             count++;
         }
-        System.out.println(count);
+        return count;
     }
 }
