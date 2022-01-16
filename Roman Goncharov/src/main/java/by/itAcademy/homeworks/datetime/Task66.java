1513f712f2e3dd0f525112dd946b502215816d4e
Roma <roma.goncharov.1997@mail.ru>
2022-01-16 16:13:37 +0300

    Completed Task64-Task66

--- Roman Goncharov/src/main/java/by/itAcademy/homeworks/datetime/Task66.java
+++ Roman Goncharov/src/main/java/by/itAcademy/homeworks/datetime/Task66.java
@@ -1,4 +1,22 @@
 package by.itAcademy.homeworks.datetime;
 
+/**
+ * Задание 66
+ * От текущей даты вывести на экран дату, которая была 60 дней назад.
+ */
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Task66 {
+    public static void main(String[] args) {
+        pastDateDisplay(60);
+    }
+
+    public static void pastDateDisplay(int days) {
+        LocalDate today = LocalDate.now();
+        System.out.println("Текущая дата: " + today.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+        System.out.println("Дата, которая была " + days + " дней назад: " +
+                today.minusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+    }
 }
