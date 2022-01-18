c4d86498a658701a1bf8e3b020bdb48fcfac9637
Olga Makarevich <olgagomel17@mail.ru>
2022-01-18 23:11:40 +0300

    Task 64 строка 25 - ?

--- Olga Makarevich/src/main/java/by/itAcademy/homeworks/datetime/Task64.java
+++ Olga Makarevich/src/main/java/by/itAcademy/homeworks/datetime/Task64.java
@@ -1,4 +1,28 @@
 package by.itAcademy.homeworks.datetime;
 
+//Ввести с клавиатуры номер месяца текущего года. Вывести на экран все его даты в формате d::MMM::uuuu.
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Scanner;
+
 public class Task64 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите номер месяца");
+        int i = scanner.nextInt();
+        printDates(i);
+    }
+
+    public static void printDates(int i) {
+        LocalDate date = LocalDate.now();
+        LocalDate date1 = date.withMonth(i);
+        LocalDate a = date1.with(TemporalAdjusters.firstDayOfMonth());
+        LocalDate b = date1.with(TemporalAdjusters.lastDayOfMonth());
+        while (a.isBefore(b)) {
+            System.out.println(a.format(DateTimeFormatter.ofPattern("d::MMM::uuuu")));
+            a.plusDays(10);
+        }
+    }
 }
