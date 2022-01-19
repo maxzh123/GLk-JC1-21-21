5004774e344ea1ea8309c60552b9611e7c387591
NickolaiDemidovich <Nicknewb97@gmail.com>
2022-01-19 13:22:22 +0300

    Complete 53/54

--- Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/threads/Task53.java
+++ Nikolai Dzemidovich/src/main/java/by/itAcademy/homeworks/threads/Task53.java
@@ -1,4 +1,14 @@
 package by.itAcademy.homeworks.threads;
 
 public class Task53 {
+    public static void main(String[] args) {
+
+        int numberOfTreads = 1;
+        MyThread53[] thread = new MyThread53[numberOfTreads];
+
+        for (int i = 0; i < thread.length; i++) {
+            thread[i] = new MyThread53();
+            thread[i].start();
+        }
+    }
 }
