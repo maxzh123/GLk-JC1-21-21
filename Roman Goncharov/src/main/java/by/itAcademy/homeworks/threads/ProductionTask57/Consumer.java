13d2da38673417a6373363bbf32cd58f7b9edc2b
Roma <roma.goncharov.1997@mail.ru>
2022-01-20 13:56:10 +0300

    Task57 Updated

--- Roman Goncharov/src/main/java/by/itAcademy/homeworks/threads/ProductionTask57/Consumer.java
+++ Roman Goncharov/src/main/java/by/itAcademy/homeworks/threads/ProductionTask57/Consumer.java
@@ -7,6 +7,7 @@ import java.util.Queue;
 public class Consumer implements Runnable {
     private final Queue<Integer> integerQueue;
     private int maxQueueSize;
+    private int myConsume;
 
     public Consumer(Queue<Integer> integerQueue, int maxQueueSize) {
         this.integerQueue = integerQueue;
@@ -17,26 +18,39 @@ public class Consumer implements Runnable {
     public void run() {
         while (Task57.processedElements <= 10000) {
             try {
+                while (integerQueue.size() == 0) {
+                    System.out.println("\nПродукция закончилась. Ждём пока добавят.\n");
+                    synchronized (integerQueue) {
+                        integerQueue.wait();
+                    }
+                }
                 consume();
-                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+        checkResult();
     }
 
-    public void consume() throws InterruptedException {
+    public void consume() {
         synchronized (integerQueue) {
-            while (integerQueue.size() == 0) {
-                System.out.println("\nПродукция закончилась. Ждём пока добавят.\n");
-                integerQueue.wait();
-            }
             System.out.println(Thread.currentThread().getName() + " взял " + integerQueue.poll() +
-                    ", количество обработанных элементов: " + ++Task57.processedElements);
-            if (integerQueue.size() <= 80) {
+                    ", это его " + ++myConsume + " купленная продукция, общее " +
+                    "количество обработанных элементов: " + ++Task57.processedElements);
+            while (integerQueue.size() <= 80) {
                 Producer.wakeUp(); // Производители могут снова производить продукцию
                 integerQueue.notifyAll();
+                break;
             }
         }
+    }
+
+    public void checkResult() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + ": итого купленной продукции " + myConsume);
     }
 }
