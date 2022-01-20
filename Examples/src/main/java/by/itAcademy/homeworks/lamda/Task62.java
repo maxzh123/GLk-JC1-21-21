8c9434dd95b7139b782e2e2b83f92070c43a4ec2
ItAcademy <none@example.com>
2022-01-20 21:19:07 +0300

    Примеры

--- Examples/src/main/java/by/itAcademy/homeworks/lamda/Task62.java
+++ Examples/src/main/java/by/itAcademy/homeworks/lamda/Task62.java
@@ -1,4 +1,24 @@
 package by.itAcademy.homeworks.lamda;
 
+import java.util.Random;
+import java.util.stream.Stream;
+
 public class Task62 {
+    public static void main(String[] args) {
+        long count = Stream.generate(() -> new Random().nextInt(255))
+                .limit(100)
+                .filter(intValue -> intValue != 0)
+                .map(intValue -> (char) intValue.intValue())
+                //.peek(System.out::print)
+                .peek(Task62::method)
+                .count();
+        //.reduce(0,(sum,streamItem)->sum+streamItem);
+        System.out.println("");
+        System.out.println(count);
+    }
+
+    static void method(int arg){
+        System.out.print(arg);
+        System.out.print("; ");
+    }
 }
