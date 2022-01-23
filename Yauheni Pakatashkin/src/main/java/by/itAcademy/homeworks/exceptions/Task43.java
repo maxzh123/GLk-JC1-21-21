18fb419cb4128012bae9c5ffbb6eafed24ec10c0
Yauheni  Pakatashkin <stinkor06@mail.ru>
2022-01-24 00:08:57 +0300

    task 43

--- Yauheni Pakatashkin/src/main/java/by/itAcademy/homeworks/exceptions/Task43.java
+++ Yauheni Pakatashkin/src/main/java/by/itAcademy/homeworks/exceptions/Task43.java
@@ -1,4 +1,18 @@
 package by.itAcademy.homeworks.exceptions;
 
 public class Task43 {
+    public static void main(String[] args) {
+        String s=null;
+      try {
+          System.out.println("Начало работы");
+          System.out.println(s.length());
+      }
+      catch (NullPointerException e){
+          System.out.println("Произошел проброс исключения");
+      }
+      finally {
+          System.out.println("Продолжение работы при необходимости");
+      }
+    }
+
 }
