b1a2f3a873f45b0a0e7eca79c33b7b73e9def621
Roma <roma.goncharov.1997@mail.ru>
2022-01-25 19:35:23 +0300

    Task68 Completed

---
+++ Roman Goncharov/src/main/java/by/itAcademy/homeworks/patterns/Task68Objects/Builder.java
@@ -0,0 +1,13 @@
+package by.itAcademy.homeworks.patterns.Task68Objects;
+
+public interface Builder {
+    void setGender(Gender gender);
+
+    void setName(String name);
+
+    void setSurname(String surname);
+
+    void setYearOfBirth(int yearOfBirth);
+
+    void setTemperamentType(TemperamentType temperamentType);
+}
---
+++ Roman Goncharov/src/main/java/by/itAcademy/homeworks/patterns/Task68Objects/TemperamentType.java
@@ -0,0 +1,5 @@
+package by.itAcademy.homeworks.patterns.Task68Objects;
+
+public enum TemperamentType {
+    CHOLERIC, SANGUINE, PHLEGMATIC, MELANCHOLIC
+}
