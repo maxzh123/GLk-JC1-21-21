360dc4d050d53ee0915a3757a576024930519950
Sergey Khomiakov <tnkmaze4.0@gmail.com>
2022-01-31 13:00:47 +0300

    Complete Tasks 67-68

--- Sergey Khomiakov/src/main/java/by/itAcademy/homeworks/patterns/Task67.java
+++ Sergey Khomiakov/src/main/java/by/itAcademy/homeworks/patterns/Task67.java
@@ -1,4 +1,14 @@
 package by.itAcademy.homeworks.patterns;
 
+/** Задание 67
+ * 1) Создать простейший сервис с методом, который выводит на экран текущуюдату.
+ * 2) Сделайте его Singleton и используйте в основном телепрограммы.
+  */
+
+
 public class Task67 {
+    public static void main(String[] args) {
+        MySingletoneDate msd = MySingletoneDate.getInstance();
+        System.out.println("Текущая дата: " + msd.showCurrentDate());
+    }
 }
